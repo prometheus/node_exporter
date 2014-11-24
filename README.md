211ddf33f1e7ea553de9de0e2ce2a0dc899171bb
Tobias Schmidt <ts@soundcloud.com>
2014-11-24 17:15:13 -0500

    Consolidate collector selection
    Remove special tags necessary for gmond and runit collectors. All
    collectors get built. Selection of which collectors to use continues to
    happen via parameter.

--- README.md
+++ README.md
@@ -2,33 +2,7 @@
 
 Prometheus exporter with pluggable metric collectors.
 
+## collectors
 
-
-## Available collectors
-
-By default the build will only include the native collectors
-that expose information from /proc.
-
-To include other collectors, specify the build tags lile this:
-
-    go build -tags 'ganglia runit' node_exporter.go
-
-
-Which collectors are used is controlled by the --enabledCollectors flag.
-
-### NativeCollector
-
-Provides metrics for load, seconds since last login and a list of tags
-read from `node_exporter.conf`.
-
-
-### GmondCollector (tag: ganglia)
-
-Talks to a local gmond and provide it's metrics.
-
-
-### RunitCollector (tag: runit)
-
-Provides metrics for each runit services like state and how long it
-has been in that state.
-
+Which collectors are used is controlled by the --enabledCollectors flag. To see
+all available collectors, use `--printCollectors`.
--- collector/gmond.go
+++ collector/gmond.go
@@ -1,4 +1,4 @@
-// +build ganglia
+// +build !nogmond
 
 package collector
 
@@ -24,7 +24,7 @@ const (
 )
 
 type gmondCollector struct {
-	Metrics map[string]*prometheus.GaugeVec
+	metrics map[string]*prometheus.GaugeVec
 	config  Config
 }
 
@@ -38,39 +38,10 @@ var illegalCharsRE = regexp.MustCompile(`[^a-zA-Z0-9_]`)
 func NewGmondCollector(config Config) (Collector, error) {
 	c := gmondCollector{
 		config:  config,
-		Metrics: map[string]*prometheus.GaugeVec{},
+		metrics: map[string]*prometheus.GaugeVec{},
 	}
 
 	return &c, nil
-}
-
-func (c *gmondCollector) setMetric(name, cluster string, metric ganglia.Metric) {
-	if _, ok := c.Metrics[name]; !ok {
-		var desc string
-		var title string
-		for _, element := range metric.ExtraData.ExtraElements {
-			switch element.Name {
-			case "DESC":
-				desc = element.Val
-			case "TITLE":
-				title = element.Val
-			}
-			if title != "" && desc != "" {
-				break
-			}
-		}
-		glog.V(1).Infof("Register %s: %s", name, desc)
-		gv := prometheus.NewGaugeVec(
-			prometheus.GaugeOpts{
-				Namespace: gangliaMetricsPrefix,
-				Name:      name,
-				Help:      desc,
-			},
-			[]string{"cluster"},
-		)
-	}
-	glog.V(1).Infof("Set %s{cluster=%q}: %f", name, cluster, metric.Value)
-	c.Metrics[name].WithLabelValues(cluster).Set(metric.Value)
 }
 
 func (c *gmondCollector) Update(ch chan<- prometheus.Metric) (err error) {
@@ -100,10 +71,39 @@ func (c *gmondCollector) Update(ch chan<- prometheus.Metric) (err error) {
 			}
 		}
 	}
-	for _, m := range c.Metrics {
+	for _, m := range c.metrics {
 		m.Collect(ch)
 	}
 	return err
+}
+
+func (c *gmondCollector) setMetric(name, cluster string, metric ganglia.Metric) {
+	if _, ok := c.metrics[name]; !ok {
+		var desc string
+		var title string
+		for _, element := range metric.ExtraData.ExtraElements {
+			switch element.Name {
+			case "DESC":
+				desc = element.Val
+			case "TITLE":
+				title = element.Val
+			}
+			if title != "" && desc != "" {
+				break
+			}
+		}
+		glog.V(1).Infof("Register %s: %s", name, desc)
+		c.metrics[name] = prometheus.NewGaugeVec(
+			prometheus.GaugeOpts{
+				Namespace: gangliaMetricsPrefix,
+				Name:      name,
+				Help:      desc,
+			},
+			[]string{"cluster"},
+		)
+	}
+	glog.V(1).Infof("Set %s{cluster=%q}: %f", name, cluster, metric.Value)
+	c.metrics[name].WithLabelValues(cluster).Set(metric.Value)
 }
 
 func toUtf8(charset string, input io.Reader) (io.Reader, error) {
