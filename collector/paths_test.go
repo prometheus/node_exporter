85eb47197fb7fda72d5ae333125883cf21c81f67
Matthias Rampke <matthias@rampke.de>
2015-09-26 19:38:31 +0200

    Add a flag for the sysfs location.
    Analogous to `collector.procfs`, but for `/sys`. Add tests for both.

---
+++ collector/paths_test.go
@@ -0,0 +1,77 @@
+// Copyright 2015 The Prometheus Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package collector
+
+import (
+	"flag"
+	"testing"
+
+	"github.com/prometheus/procfs"
+)
+
+func TestDefaultProcPath(t *testing.T) {
+	if err := flag.Set("collector.procfs", procfs.DefaultMountPoint); err != nil {
+		t.Fatal(err)
+	}
+
+	if got, want := procFilePath("somefile"), "/proc/somefile"; got != want {
+		t.Errorf("Expected: %s, Got: %s", want, got)
+	}
+
+	if got, want := procFilePath("some/file"), "/proc/some/file"; got != want {
+		t.Errorf("Expected: %s, Got: %s", want, got)
+	}
+}
+
+func TestCustomProcPath(t *testing.T) {
+	if err := flag.Set("collector.procfs", "./../some/./place/"); err != nil {
+		t.Fatal(err)
+	}
+
+	if got, want := procFilePath("somefile"), "../some/place/somefile"; got != want {
+		t.Errorf("Expected: %s, Got: %s", want, got)
+	}
+
+	if got, want := procFilePath("some/file"), "../some/place/some/file"; got != want {
+		t.Errorf("Expected: %s, Got: %s", want, got)
+	}
+}
+
+func TestDefaultSysPath(t *testing.T) {
+	if err := flag.Set("collector.sysfs", "/sys"); err != nil {
+		t.Fatal(err)
+	}
+
+	if got, want := sysFilePath("somefile"), "/sys/somefile"; got != want {
+		t.Errorf("Expected: %s, Got: %s", want, got)
+	}
+
+	if got, want := sysFilePath("some/file"), "/sys/some/file"; got != want {
+		t.Errorf("Expected: %s, Got: %s", want, got)
+	}
+}
+
+func TestCustomSysPath(t *testing.T) {
+	if err := flag.Set("collector.sysfs", "./../some/./place/"); err != nil {
+		t.Fatal(err)
+	}
+
+	if got, want := sysFilePath("somefile"), "../some/place/somefile"; got != want {
+		t.Errorf("Expected: %s, Got: %s", want, got)
+	}
+
+	if got, want := sysFilePath("some/file"), "../some/place/some/file"; got != want {
+		t.Errorf("Expected: %s, Got: %s", want, got)
+	}
+}
