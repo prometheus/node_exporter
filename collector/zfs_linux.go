809afd976d9ac2140c9f480e764faccc113b4b8c
Christian Schwarz <me@cschwarz.com>
2016-02-25 13:55:02 +0100

    Add a collector for ZFS, currently focussed on ARC stats.
    It is tested on FreeBSD 10.2-RELEASE and Linux (ZFS on Linux 0.6.5.4).
    
    On FreeBSD, Solaris, etc. ZFS metrics are exposed through sysctls.
    ZFS on Linux exposes the same metrics through procfs `/proc/spl/...`.
    
    In addition to sysctl metrics, 'computed metrics' are exposed by
    the collector, which are based on several sysctl values.
    There is some conditional logic involved in computing these metrics
    which cannot be easily mapped to PromQL.
    
    Not all 92 ARC sysctls are exposed right now but this can be changed
    with one additional LOC each.

---
+++ collector/zfs_linux.go
@@ -0,0 +1,83 @@
+package collector
+
+// +build linux
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/prometheus/common/log"
+)
+
+const zfsArcstatsProcpath = "spl/kstat/zfs/arcstats"
+
+func zfsInitialize() error {
+	path := procFilePath(zfsArcstatsProcpath)
+	fd, err := os.Open(path)
+	if err != nil {
+		log.Errorf("Cannot open ZFS arcstats logpath: %s", path)
+		return err
+	}
+	return fd.Close()
+}
+
+func (p *zfsMetricProvider) PrepareUpdate() (err error) {
+
+	err = p.prepareUpdateArcstats(zfsArcstatsProcpath)
+	if err != nil {
+		return
+	}
+	return nil
+}
+
+func (p *zfsMetricProvider) handleFetchedMetricCacheMiss(s zfsSysctl) (value zfsMetricValue, err error) {
+
+	// all values are fetched in PrepareUpdate()
+	return zfsErrorValue, fmt.Errorf("sysctl '%s' found")
+}
+
+func (p *zfsMetricProvider) prepareUpdateArcstats(zfsArcstatsProcpath string) (err error) {
+
+	file, err := os.Open(procFilePath(zfsArcstatsProcpath))
+	if err != nil {
+		log.Errorf("Cannot open ZFS arcstats procfs file for reading. " +
+			" Is the kernel module mounted?")
+		return err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	parseLine := false
+	for scanner.Scan() {
+		parts := strings.Fields(scanner.Text())
+
+		if !parseLine && parts[0] == "name" && parts[1] == "type" && parts[2] == "data" {
+			// Start parsing from here
+			parseLine = true
+			continue
+		}
+
+		if !parseLine || len(parts) < 3 {
+			continue
+		}
+
+		key := fmt.Sprintf("kstat.zfs.misc.arcstats.%s", parts[0])
+
+		value, err := strconv.Atoi(parts[2])
+		if err != nil {
+			return fmt.Errorf("could not parse expected integer value for '%s'", key)
+		}
+		log.Debugf("%s = %d", key, value)
+		p.fetchedResults[zfsSysctl(key)] = zfsMetricValue(value)
+	}
+	if !parseLine {
+		return errors.New("did not parse a single arcstat metrics")
+	}
+
+	return scanner.Err()
+}
