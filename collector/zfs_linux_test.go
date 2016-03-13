3c33e7ede406dc7782a6663e44e4f5438d4585b8
Christian Schwarz <me@cschwarz.com>
2016-03-13 13:09:08 +0100

    Add unit test for arcstats parsing.

---
+++ collector/zfs_linux_test.go
@@ -0,0 +1,36 @@
+package collector
+
+import (
+	"os"
+	"testing"
+)
+
+func TestArcstatsParsing(t *testing.T) {
+
+	arcstatsFile, err := os.Open("fixtures/proc/spl/kstat/zfs/arcstats")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer arcstatsFile.Close()
+
+
+	p := NewZFSMetricProvider()
+	err = p.parseArcstatsProcfsFile(arcstatsFile)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Logf("Parsed values mapped to sysctls:\n%v", p.values)
+
+	value, err := p.Value(zfsSysctl("kstat.zfs.misc.arcstats.hits"))
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if value != zfsMetricValue(8772612) {
+		t.Fatalf("Incorrect value parsed from procfs data")
+	}
+
+}
