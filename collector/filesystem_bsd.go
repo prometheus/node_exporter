4b04d04558e596602eb3896b261f2412f4c7d1c9
Johannes 'fish' Ziemke <github@freigeist.org>
2016-07-05 18:56:11 +0200

    Enable *bsd collector on darwin

--- collector/filesystem_bsd.go
+++ collector/filesystem_bsd.go
@@ -11,7 +11,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build freebsd openbsd
+// +build freebsd openbsd darwin,amd64
 // +build !nofilesystem
 
 package collector
