10ba27bf2c6314ea42c140167cc6e069ac99bc8c
Corey Stewart <cbstewart9393@gmail.com>
2016-12-08 10:06:53 -0500

    Remove FreeBSD support for zfs plugin.
    This also involves removing zfs_zpool code for now.
    
    Signed-Off-By: Corey Stewart <stewa169@purdue.edu>
    Signed-Off-By: Joe Handzik <joseph.t.handzik@hpe.com>

--- collector/zfs.go
+++ collector/zfs.go
@@ -11,10 +11,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package collector
-
-// +build linux freebsd
+// +build linux
 // +build !nozfs
+
+package collector
 
 import (
 	"errors"
