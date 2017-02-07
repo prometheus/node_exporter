55417d768820ec44c5f995182cc808640dfd1667
Thorhallur Sverrisson <thorhallur.sverrisson@sabre.com>
2017-02-07 08:30:49 -0600

    Moving buddyinfo logic to procfs

--- collector/buddyinfo.go
+++ collector/buddyinfo.go
@@ -1,4 +1,4 @@
-// Copyright 2015 The Prometheus Authors
+// Copyright 2017 The Prometheus Authors
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -17,18 +17,13 @@
 package collector
 
 import (
-	"bufio"
 	"fmt"
-	"io"
-	"os"
 	"strconv"
-	"strings"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/common/log"
+	"github.com/prometheus/procfs"
 )
-
-type buddyInfo map[string]map[string][]float64
 
 const (
 	buddyInfoSubsystem = "buddyinfo"
@@ -55,10 +50,16 @@ func NewBuddyinfoCollector() (Collector, error) {
 // Update calls (*buddyinfoCollector).getBuddyInfo to get the platform specific
 // buddyinfo metrics.
 func (c *buddyinfoCollector) Update(ch chan<- prometheus.Metric) (err error) {
-	buddyInfo, err := c.getBuddyInfo()
+	fs, err := procfs.NewFS(*procPath)
+	if err != nil {
+		return fmt.Errorf("failed to open procfs: %v", err)
+	}
+
+	buddyInfo, err := fs.NewBuddyInfo()
 	if err != nil {
 		return fmt.Errorf("couldn't get buddyinfo: %s", err)
 	}
+
 	log.Debugf("Set node_buddy: %#v", buddyInfo)
 	for node, zones := range buddyInfo {
 		for zone, values := range zones {
@@ -72,43 +73,4 @@ func (c *buddyinfoCollector) Update(ch chan<- prometheus.Metric) (err error) {
 		}
 	}
 	return nil
-}
-func (c *buddyinfoCollector) getBuddyInfo() (buddyInfo, error) {
-	file, err := os.Open(procFilePath("buddyinfo"))
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-
-	return parseBuddyInfo(file)
-}
-
-func parseBuddyInfo(r io.Reader) (buddyInfo, error) {
-	var (
-		buddyInfo = buddyInfo{}
-		scanner   = bufio.NewScanner(r)
-	)
-
-	for scanner.Scan() {
-		var err error
-		line := scanner.Text()
-		parts := strings.Fields(string(line))
-		node := strings.TrimRight(parts[1], ",")
-		zone := strings.TrimRight(parts[3], ",")
-		arraySize := len(parts[4:])
-		sizes := make([]float64, arraySize)
-		for i := 0; i < arraySize; i++ {
-			sizes[i], err = strconv.ParseFloat(parts[i+4], 64)
-			if err != nil {
-				return nil, fmt.Errorf("invalid value in buddyinfo: %s", err)
-			}
-		}
-
-		if _, ok := buddyInfo[node]; !ok {
-			buddyInfo[node] = make(map[string][]float64)
-		}
-		buddyInfo[node][zone] = sizes
-	}
-
-	return buddyInfo, nil
 }
