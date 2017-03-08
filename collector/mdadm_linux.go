8efa6e922318844706d16d0aaedb345006027b77
Birger Schmidt <bs-github@netgaroo.com>
2017-03-08 08:08:23 +0100

    refactor mdadm collector to use syscall intead of mdstats parsing
    This is the turn away from parsing /proc/mdstat to gather information
    about the md RAID status and the using the GET_ARRAY_INFO call instead.
    
    - this has to be tested on real RAIDs
    - this has to be tested on other architectures than x86_64
    - needed tests are still missing and therefore disabled
    - information about blocks (total/synced) still come from mdstat
      because those values are not present in ARRAY_INFO

--- collector/mdadm_linux.go
+++ collector/mdadm_linux.go
@@ -23,28 +23,67 @@ import (
 	"strconv"
 	"strings"
 
+	"syscall"
+	"unsafe"
+
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/common/log"
 )
 
 var (
-	statuslineRE             = regexp.MustCompile(`(\d+) blocks .*\[(\d+)/(\d+)\] \[[U_]+\]`)
-	raid0lineRE              = regexp.MustCompile(`(\d+) blocks( super ([0-9\.])*)? \d+k chunks`)
-	buildlineRE              = regexp.MustCompile(`\((\d+)/\d+\)`)
-	unknownPersonalityLineRE = regexp.MustCompile(`(\d+) blocks (.*)`)
-	raidPersonalityRE        = regexp.MustCompile(`raid[0-9]+`)
+	statuslineRE = regexp.MustCompile(`(\d+) blocks`)
+	buildlineRE  = regexp.MustCompile(`\((\d+)/\d+\)`)
 )
 
 type mdStatus struct {
-	mdName       string
-	isActive     bool
+	name         string
+	active       bool
 	disksActive  int64
 	disksFailed  int64
+	disksMissing int64
 	disksSpare   int64
-	disksTotal   int64
 	blocksTotal  int64
 	blocksSynced int64
 }
+
+type (
+	mdu_array_info struct {
+		// Generic constant information
+		major_version  int32
+		minor_version  int32
+		patch_version  int32
+		ctime          uint32
+		level          int32 //  RAID level
+		size           int32 //  useless - only works for disk sizes that fit in an int32
+		nr_disks       int32 //  Number of physically present disks. (working + failed)
+		raid_disks     int32 //  Number of disks in the RAID.
+		md_minor       int32 //     This may be larger than "nr" if disks are missing
+		not_persistent int32 //     and smaller than "nr" when spare disks are around.
+		// Generic state information
+		utime         uint32 //  Superblock update time
+		state         int32  //  State bits (clean, ...)
+		active_disks  int32  //  Number of active (in sync) disks
+		working_disks int32  //  Number of working disks (active + sync)
+		failed_disks  int32  //  Number of failed disks
+		spare_disks   int32  //  Number of spare (stand-by) disks
+		// Personality information
+		layout     int32 //  the array's physical layout
+		chunk_size int32 //  chunk size in bytes
+	}
+)
+
+const (
+	// _IOR definition:
+	// https://github.com/torvalds/linux/blob/e067eba5871c6922539dc1728699c14e6b22590f/include/uapi/asm-generic/ioctl.h#L85
+	// from major.h
+	// #define MD_MAJOR                9
+	// from md_u.h
+	// #define GET_ARRAY_INFO          _IOR (MD_MAJOR, 0x11, mdu_array_info_t)
+
+	GET_ARRAY_INFO = (0x8048 << 16) | 0x0911
+	// No idea what the 0x8048 means
+	// I've just printed the const in a piece of C code on x86_64
+)
 
 type mdadmCollector struct{}
 
@@ -52,87 +91,76 @@ func init() {
 	Factories["mdadm"] = NewMdadmCollector
 }
 
-func evalStatusline(statusline string) (active, total, size int64, err error) {
+func max(a, b int32) int32 {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+// evalStatusline gets the number of blocks of the device out of the status-line.
+func evalStatusline(statusline string) (blocksTotal int64, err error) {
 	matches := statuslineRE.FindStringSubmatch(statusline)
 
 	// +1 to make it more obvious that the whole string containing the info is also returned as matches[0].
-	if len(matches) < 3+1 {
-		return 0, 0, 0, fmt.Errorf("too few matches found in statusline: %s", statusline)
-	} else if len(matches) > 3+1 {
-		return 0, 0, 0, fmt.Errorf("too many matches found in statusline: %s", statusline)
+	if len(matches) != 1+1 {
+		return 0, fmt.Errorf("unable to find the number of blocks in statusline: %s", statusline)
 	}
 
-	size, err = strconv.ParseInt(matches[1], 10, 64)
-	if err != nil {
-		return 0, 0, 0, fmt.Errorf("%s in statusline: %s", err, statusline)
-	}
-
-	total, err = strconv.ParseInt(matches[2], 10, 64)
-	if err != nil {
-		return 0, 0, 0, fmt.Errorf("%s in statusline: %s", err, statusline)
-	}
-	active, err = strconv.ParseInt(matches[3], 10, 64)
-	if err != nil {
-		return 0, 0, 0, fmt.Errorf("%s in statusline: %s", err, statusline)
-	}
-
-	return active, total, size, nil
-}
-
-func evalRaid0line(statusline string) (size int64, err error) {
-	matches := raid0lineRE.FindStringSubmatch(statusline)
-
-	if len(matches) < 2 {
-		return 0, fmt.Errorf("invalid raid0 status line: %s", statusline)
-	}
-
-	size, err = strconv.ParseInt(matches[1], 10, 64)
+	blocksTotal, err = strconv.ParseInt(matches[1], 10, 64)
 	if err != nil {
 		return 0, fmt.Errorf("%s in statusline: %s", err, statusline)
 	}
 
-	return size, nil
-}
-
-func evalUnknownPersonalitylineRE(statusline string) (size int64, err error) {
-	matches := unknownPersonalityLineRE.FindStringSubmatch(statusline)
-
-	if len(matches) != 2+1 {
-		return 0, fmt.Errorf("invalid unknown personality status line: %s", statusline)
-	}
-
-	size, err = strconv.ParseInt(matches[1], 10, 64)
-	if err != nil {
-		return 0, fmt.Errorf("%s in statusline: %s", err, statusline)
-	}
-
-	return size, nil
+	return blocksTotal, nil
 }
 
 // evalBuildline gets the size that has already been synced out of the sync-line.
-func evalBuildline(buildline string) (int64, error) {
+func evalBuildline(buildline string) (blocksSynced int64, err error) {
 	matches := buildlineRE.FindStringSubmatch(buildline)
 
 	// +1 to make it more obvious that the whole string containing the info is also returned as matches[0].
-	if len(matches) < 1+1 {
-		return 0, fmt.Errorf("too few matches found in buildline: %s", buildline)
+	if len(matches) != 1+1 {
+		return 0, fmt.Errorf("unable to find the rebuild block count in buildline: %s", buildline)
 	}
 
-	if len(matches) > 1+1 {
-		return 0, fmt.Errorf("too many matches found in buildline: %s", buildline)
-	}
-
-	syncedSize, err := strconv.ParseInt(matches[1], 10, 64)
+	blocksSynced, err = strconv.ParseInt(matches[1], 10, 64)
 
 	if err != nil {
 		return 0, fmt.Errorf("%s in buildline: %s", err, buildline)
 	}
 
-	return syncedSize, nil
+	return blocksSynced, nil
+}
+
+// getArrayInfo gets RAID info via the GET_ARRAY_INFO IOCTL syscall.
+func getArrayInfo(md *mdStatus) (err error) {
+	// needs md.name as input
+	var array mdu_array_info
+	mdDev := "/dev/" + md.name
+	file, err := os.Open(mdDev)
+	if err != nil {
+		return fmt.Errorf("error opening %s: %s", mdDev, err)
+	}
+	fd := file.Fd() // get the unix descriptor
+	_, _, errno := syscall.Syscall(syscall.SYS_IOCTL, fd, GET_ARRAY_INFO, uintptr(unsafe.Pointer(&array)))
+	file.Close()
+	if errno != 0 {
+		return fmt.Errorf("error getting RAID info via IOCTL syscall for %s: %s", mdDev, errno)
+	}
+	md.disksActive = int64(array.active_disks)
+	md.disksFailed = int64(array.failed_disks)
+	md.disksMissing = int64(max(0, array.raid_disks-array.nr_disks))
+	md.disksSpare = int64(array.spare_disks)
+	return nil
 }
 
 // parseMdstat parses an mdstat-file and returns a struct with the relevant infos.
 func parseMdstat(mdStatusFilePath string) ([]mdStatus, error) {
+	// TODO
+	// we should probably get rid of parsing the mdstat file at all,
+	// but this file in /proc does not depend on the underlying architecture
+	// while the ioctl could.
 	content, err := ioutil.ReadFile(mdStatusFilePath)
 	if err != nil {
 		return []mdStatus{}, fmt.Errorf("error parsing mdstat: %s", err)
@@ -141,11 +169,6 @@ func parseMdstat(mdStatusFilePath string) ([]mdStatus, error) {
 	mdStatusFile := string(content)
 
 	lines := strings.Split(mdStatusFile, "\n")
-	var (
-		currentMD                          string
-		personality                        string
-		active, failed, spare, total, size int64
-	)
 
 	// Each md has at least the deviceline, statusline and one empty line afterwards
 	// so we will have probably something of the order len(lines)/3 devices
@@ -154,7 +177,6 @@ func parseMdstat(mdStatusFilePath string) ([]mdStatus, error) {
 	mdStates := make([]mdStatus, 0, estimateMDs)
 
 	for i, l := range lines {
-		active, failed, spare, total, size = 0, 0, 0, 0, 0
 
 		if l == "" {
 			// Skip entirely empty lines.
@@ -175,42 +197,33 @@ func parseMdstat(mdStatusFilePath string) ([]mdStatus, error) {
 		if len(mainLine) < 4 {
 			return mdStates, fmt.Errorf("error parsing mdline: %s", l)
 		}
-		currentMD = mainLine[0]               // The name of the md-device.
-		isActive := (mainLine[2] == "active") // The activity status of the md-device.
-		personality = ""
-		for _, possiblePersonality := range mainLine[3:] {
-			if raidPersonalityRE.MatchString(possiblePersonality) {
-				personality = possiblePersonality
-				break
-			}
+
+		var md mdStatus
+
+		// mainLine[0] is the name of the md-device.
+		// we have to set name before calling getArrayInfo
+		md.name = mainLine[0]
+
+		err := getArrayInfo(&md)
+		if err != nil {
+			return mdStates, err
 		}
-		failed = int64(strings.Count(l, "(F)"))
-		spare = int64(strings.Count(l, "(S)"))
+
+		// mainLine[2], either "active" or "inactive"
+		md.active = mainLine[2] == "active"
 
 		if len(lines) <= i+3 {
-			return mdStates, fmt.Errorf("error parsing mdstat: entry for %s has fewer lines than expected", currentMD)
+			return mdStates, fmt.Errorf("error parsing mdstat: entry for %s has fewer lines than expected", md.name)
 		}
 
-		switch {
-		case personality == "raid0":
-			active = int64(len(mainLine) - 4)     // Get the number of devices from the main line.
-			total = active                        // Raid0 active and total is always the same if active.
-			size, err = evalRaid0line(lines[i+1]) // Parse statusline, always present.
-		case raidPersonalityRE.MatchString(personality):
-			active, total, size, err = evalStatusline(lines[i+1]) // Parse statusline, always present.
-		default:
-			log.Infof("Personality unknown: %s\n", mainLine)
-			size, err = evalUnknownPersonalitylineRE(lines[i+1]) // Parse statusline, always present.
-		}
-
+		md.blocksTotal, err = evalStatusline(lines[i+1]) // Parse statusline, always present.
 		if err != nil {
 			return mdStates, fmt.Errorf("error parsing mdstat: %s", err)
 		}
 
 		// Now get the number of synced blocks.
-		var syncedBlocks int64
 
-		// Get the line number of the syncing-line.
+		// Get the line number of the sync-line.
 		var j int
 		if strings.Contains(lines[i+2], "bitmap") { // then skip the bitmap line
 			j = i + 3
@@ -223,15 +236,15 @@ func parseMdstat(mdStatusFilePath string) ([]mdStatus, error) {
 		if strings.Contains(lines[j], "recovery") ||
 			strings.Contains(lines[j], "resync") &&
 				!strings.Contains(lines[j], "\tresync=") {
-			syncedBlocks, err = evalBuildline(lines[j])
+			md.blocksSynced, err = evalBuildline(lines[j])
 			if err != nil {
 				return mdStates, fmt.Errorf("error parsing mdstat: %s", err)
 			}
 		} else {
-			syncedBlocks = size
+			md.blocksSynced = md.blocksTotal
 		}
 
-		mdStates = append(mdStates, mdStatus{currentMD, isActive, active, failed, spare, total, size, syncedBlocks})
+		mdStates = append(mdStates, md)
 
 	}
 
@@ -253,7 +266,7 @@ var (
 
 	disksDesc = prometheus.NewDesc(
 		prometheus.BuildFQName(Namespace, "md", "disks"),
-		"Number of disks of device. (state=\"any\" equals to the number of disks in the array)",
+		"Number of disks in RAID. (total = active + failed + missing)",
 		[]string{"device", "state"},
 		nil,
 	)
@@ -295,9 +308,9 @@ func (c *mdadmCollector) Update(ch chan<- prometheus.Metric) error {
 	var isActiveFloat float64
 	for _, mds := range mdstate {
 
-		log.Debugf("collecting metrics for device %s", mds.mdName)
+		log.Debugf("collecting metrics for device %s", mds.name)
 
-		if mds.isActive {
+		if mds.active {
 			isActiveFloat = 1
 		} else {
 			isActiveFloat = 0
@@ -307,49 +320,49 @@ func (c *mdadmCollector) Update(ch chan<- prometheus.Metric) error {
 			isActiveDesc,
 			prometheus.GaugeValue,
 			isActiveFloat,
-			mds.mdName,
+			mds.name,
 		)
 
 		ch <- prometheus.MustNewConstMetric(
 			disksDesc,
 			prometheus.GaugeValue,
 			float64(mds.disksActive),
-			mds.mdName, "active",
+			mds.name, "active",
 		)
 
 		ch <- prometheus.MustNewConstMetric(
 			disksDesc,
 			prometheus.GaugeValue,
 			float64(mds.disksFailed),
-			mds.mdName, "failed",
+			mds.name, "failed",
+		)
+
+		ch <- prometheus.MustNewConstMetric(
+			disksDesc,
+			prometheus.GaugeValue,
+			float64(mds.disksMissing),
+			mds.name, "missing",
 		)
 
 		ch <- prometheus.MustNewConstMetric(
 			disksDesc,
 			prometheus.GaugeValue,
 			float64(mds.disksSpare),
-			mds.mdName, "spare",
-		)
-
-		ch <- prometheus.MustNewConstMetric(
-			disksDesc,
-			prometheus.GaugeValue,
-			float64(mds.disksTotal),
-			mds.mdName, "any",
+			mds.name, "spare",
 		)
 
 		ch <- prometheus.MustNewConstMetric(
 			blocksTotalDesc,
 			prometheus.GaugeValue,
 			float64(mds.blocksTotal),
-			mds.mdName,
+			mds.name,
 		)
 
 		ch <- prometheus.MustNewConstMetric(
 			blocksSyncedDesc,
 			prometheus.GaugeValue,
 			float64(mds.blocksSynced),
-			mds.mdName,
+			mds.name,
 		)
 
 	}
