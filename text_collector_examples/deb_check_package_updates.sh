ba93189d158bfa85e75c0241250ceef8f360b922
Anton Strukov <ANStrukov@luxoft.com>
2017-03-30 12:53:47 +0300

    look at this new fancy

--- text_collector_examples/deb_check_package_updates.sh
+++ text_collector_examples/deb_check_package_updates.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
-/usr/bin/apt-get update
-/bin/echo "" > /var/lib/node_exporter/textfile_collector/debian_updates.prom
-/usr/bin/apt-get --just-print upgrade | /bin/grep -- Debian-Security | /bin/grep ^Inst | /usr/bin/wc -l | /bin/sed 's/^/debian_updates_pending{type="security"} /' > /var/lib/node_exporter/textfile_collector/debian_updates.prom
-/usr/bin/apt-get --just-print upgrade | /bin/grep -v -- Debian-Security | /bin/grep ^Inst | /usr/bin/wc -l | /bin/sed 's/^/debian_updates_pending{type="others"} /' >> /var/lib/node_exporter/textfile_collector/debian_updates.prom
+/usr/bin/apt-get --just-print upgrade | /usr/bin/awk '/^Inst/ {print $5}' | /usr/bin/sort  | /usr/bin/uniq -c | /usr/bin/awk '{ gsub("\\\\", "\\\\", $2); gsub("\"", "\\\"", $2); print "apt_upgrades_pending{origin=\"" $2 "\"} " $1 }' > /var/lib/node_exporter/textfile_collector/debian_updates.prom.$$
+/bin/mv /var/lib/node_exporter/textfile_collector/debian_updates.prom.$$ /var/lib/node_exporter/textfile_collector/debian_updates.prom
