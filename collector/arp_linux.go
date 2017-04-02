9fb9c963b3cf51e8dcce39a0922d6e15e1fb95c3
Sam Kottler <skottler@github.com>
2017-04-02 15:09:39 -0400

    Move arp_common into arp_linux

--- collector/arp_linux.go
+++ collector/arp_linux.go
@@ -11,6 +11,25 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 )
 
+type arpCollector struct {
+	count *prometheus.Desc
+}
+
+func init() {
+	Factories["arp"] = NewArpCollector
+}
+
+// NewArpCollector returns a new Collector exposing ARP stats.
+func NewArpCollector() (Collector, error) {
+	return &arpCollector{
+		count: prometheus.NewDesc(
+			prometheus.BuildFQName(Namespace, "arp",
+				"count"), "ARP entries by device",
+			[]string{"device"}, nil,
+		),
+	}, nil
+}
+
 func getArpEntries() (map[string]uint32, error) {
 	file, err := os.Open(procFilePath("net/arp"))
 	if err != nil {
