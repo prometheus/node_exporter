d32a3d539759cd703fc551ca90741e23992f5b64
Derek Marcotte <554b8425@razorfever.net>
2017-06-08 13:32:06 -0400

    Fix typos per review.

--- collector/memory_bsd.go
+++ collector/memory_bsd.go
@@ -120,8 +120,8 @@ func NewMemoryCollector() (Collector, error) {
 	}, nil
 }
 
-// Update checks relevant sysctls for curent memrory usage, and kvm for swap
-// useage.
+// Update checks relevant sysctls for current memory usage, and kvm for swap
+// usage.
 func (c *memoryCollector) Update(ch chan<- prometheus.Metric) error {
 	for _, m := range c.sysctls {
 		v, err := m.Value()
