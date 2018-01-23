94825125d3fb2c74668699a7541b1f660212a1c5
Pavel Kutishchev <pavel.kutishchev@olx.com>
2018-01-23 13:43:23 +0100

    Added threads gathering for linux system

---
+++ collector/linux_threads.go
@@ -0,0 +1,85 @@
+// Copyright 2015 The Prometheus Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// build+ !nothreads
+
+package collector
+
+import (
+	"fmt"
+	"io/ioutil"
+	"regexp"
+	"strings"
+	"strconv"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/common/log"
+)
+
+type threadsCollector struct {
+	threadAlloc *prometheus.Desc
+}
+func init() {
+	registerCollector("threads", defaultDisabled, NewThreadsCollector)
+}
+
+func NewThreadsCollector() (Collector, error) {
+	return &threadsCollector{
+		threadAlloc: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace,"", "threads"),
+			"Allocated thread",
+			nil, nil,
+		),
+	}, nil
+}
+func (t *threadsCollector) Update(ch chan<- prometheus.Metric) error {
+	val, err := readProcessStatus()
+	if err != nil {
+		return fmt.Errorf("Unable to retrieve number of threads %v\n", err)
+	}
+	ch <- prometheus.MustNewConstMetric(t.threadAlloc, prometheus.GaugeValue, float64(val))
+	return nil
+}
+
+func readProcessStatus() (int, error) {
+	processDir, err := regexp.Compile("([0-9]){1,8}")
+	folders, err := ioutil.ReadDir("/proc")
+	if err != nil {
+		fmt.Println(err)
+		return 0, err
+	}
+	threads := 0
+	for _, f := range folders {
+		if f.IsDir() && processDir.MatchString(f.Name()) {
+			file, err := ioutil.ReadFile("/proc/" + f.Name() + "/status")
+			if err != nil {
+				log.Error(err)
+				return 0, err
+			}
+			line := strings.Split(string(file), "\n")
+			if err != nil {
+				return 0, err
+			}
+			for _, l := range line {
+				if strings.Contains(string(l), "Threads:") {
+					threadStr := strings.Split(string(l), ":")
+					tread, err :=  strconv.Atoi(threadStr[1])
+					if err != nil {
+						return 0, err
+					}
+					threads += tread
+				}
+			}
+		}
+	}
+	return threads, nil
+}
---
+++ collector/linux_threads_test.go
@@ -0,0 +1,17 @@
+package collector
+
+import (
+	"testing"
+)
+
+func TestReadProcessStatus(t *testing.T) {
+	want := 4
+	threads, err := readProcessStatus()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if threads < want {
+		t.Fatalf("Current threads: %d Shouldn't be less than wanted %d",threads,want)
+	}
+
+}
