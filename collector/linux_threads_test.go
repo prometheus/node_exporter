aaa8518d084e9b00cd64c7b06d18c5b23f7ff10a
Pavel Kutishchev <pavel.kutishchev@olx.com>
2018-02-05 08:12:26 +0100

    Changes as per PR duscissuon

--- collector/linux_threads_test.go
+++ collector/linux_threads_test.go
@@ -8,22 +8,13 @@ func TestReadProcessStatus(t *testing.T) {
 	want := 4
 	states, threads, err := getAllocatedThreads()
 	if err != nil {
-		t.Fatalf("Cannot retrieve data from procfs getAllocatedThreads function: %v ",err)
+		t.Fatalf("Cannot retrieve data from procfs getAllocatedThreads function: %v ", err)
 	}
 	if threads < want {
-		t.Fatalf("Current threads: %d Shouldn't be less than wanted %d",threads,want)
+		t.Fatalf("Current threads: %d Shouldn't be less than wanted %d", threads, want)
 	}
-	if states ==  nil {
+	if states == nil {
 
-		t.Fatalf("Prcess states cannot be nil %v:" ,states)
+		t.Fatalf("Prcess states cannot be nil %v:", states)
 	}
-	max, err := getMaxThreads()
-	if err != nil {
-		t.Fatalf("Cannot retrieve data from procfs getMaxThreads func: %v ",err)
-	}
-	if max <= 0 {
-		t.Fatalf("Maximum allowed amount of threads in the system %d which sould be" +
-			"greated than 0",max)
-	}
-
 }
