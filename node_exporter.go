5c8a6715a91c7a8ae5efb26d2d3e5c0028b22e56
Peter Löffler <peter.loeffler@guruz.at>
2018-04-06 06:44:38 +0200

    we have to use the collectors from the runtatic fork
    Signed-off-by: Peter Löffler <peter.loeffler@runtastic.com>

--- node_exporter.go
+++ node_exporter.go
@@ -23,7 +23,7 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/prometheus/common/log"
 	"github.com/prometheus/common/version"
-	"github.com/prometheus/node_exporter/collector"
+	"github.com/runtastic/node_exporter/collector"
 	"gopkg.in/alecthomas/kingpin.v2"
 )
 
