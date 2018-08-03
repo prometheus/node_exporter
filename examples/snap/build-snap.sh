27eb9b7ae6483a1538adffd1b246f848d4dc56e4
fred.austin <fred.austin@n-dimension.com>
2018-08-03 11:05:53 -0400

    Update build script to use sudo

--- examples/snap/build-snap.sh
+++ examples/snap/build-snap.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 ######################################################################
 
-user=`whoami`
-if [ "${user}" != "root" ]; then
-   echo "This script must run as root"
-   exit -1
-fi
-
 VERSION="0.16.0"
 if [ "$#" -gt 0 ]; then
     VERSION=$1
@@ -19,13 +13,13 @@ export EXPORTER_VERSION=${VERSION}
 rm -f snapcraft.yaml
 sed -e "s/EXPORTER_VERSION/${EXPORTER_VERSION}/g" prometheus-node-exporter.yaml > snapcraft.yaml
 
-snapcraft clean prometheus-node-exporter
-rm -rf parts prime snap stage
+sudo snapcraft clean prometheus-node-exporter
+sudo rm -rf parts prime snap stage
 
 # include manifest file in SNAP file
 export SNAPCRAFT_BUILD_INFO=1
-snapcraft snap
+sudo snapcraft snap
 
 # remove temp file, does not remove build directories
-snapcraft clean prometheus-node-exporter
-rm -f snapcraft.yaml
+sudo snapcraft clean prometheus-node-exporter
+sudo rm -f snapcraft.yaml
