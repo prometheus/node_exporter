f343018d9474f9a0642a71566b067d6916649078
Sven Haardiek <sven.haardiek@iotec-gmbh.de>
2018-10-17 15:27:39 +0200

    Introduce example to get pending updates from pacman
    Signed-off-by: Sven Haardiek <sven.haardiek@iotec-gmbh.de>

---
+++ text_collector_examples/pacman.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+#
+# Description: Expose metrics from pacman updates
+# If installed The bash script *checkupdates*, included with the
+# *pacman-contrib* package, is used to calculate the number of pending updates.
+# Otherwise *pacman* is used for calculation.
+#
+# Author: Sven Haardiek <sven@haardiek.de>
+
+set -o errexit
+set -o nounset
+
+if [ -x /usr/bin/checkupdates ]
+then
+    updates=$(/usr/bin/checkupdates | wc -l)
+    cache=0
+else
+    updates=$(/usr/bin/pacman -Qu | wc -l)
+    cache=1
+fi
+
+echo "# HELP updates_pending number of pending updates, derived live or from cache"
+echo "# TYPE updates_pending gauge"
+echo "updates_pending{package_manager=\"pacman\",from_cache=\"$cache\"} $updates"
