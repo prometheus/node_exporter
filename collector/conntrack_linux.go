3ff25769e41e3eea0c8bccb559891e7510812a0e
Aleksandr Kozlov <avlkozlov@avito.ru>
2018-11-14 17:00:43 +0300

    Added conntrack statistics metrics
    Signed-off-by: Aleksandr Kozlov <avlkozlov@avito.ru>

--- collector/conntrack_linux.go
+++ collector/conntrack_linux.go
@@ -16,12 +16,30 @@
 package collector
 
 import (
+	"bufio"
+	"bytes"
 	"github.com/prometheus/client_golang/prometheus"
+	"io/ioutil"
+	"strconv"
+	"strings"
 )
 
 type conntrackCollector struct {
-	current *prometheus.Desc
-	limit   *prometheus.Desc
+	current      *prometheus.Desc
+	limit        *prometheus.Desc
+	searched     *prometheus.Desc
+	insert       *prometheus.Desc
+	insertFailed *prometheus.Desc
+	delete       *prometheus.Desc
+	drop         *prometheus.Desc
+}
+
+type ConntrackStatistics struct {
+	Searched     uint64 // Number of conntrack table lookups performed
+	Delete       uint64 // Number of conntrack entries which were removed
+	Insert       uint64 // Number of entries inserted into the list
+	InsertFailed uint64 // Number of entries for which list insertion was attempted but failed (happens if the same entry is already present)
+	Drop         uint64 // Number of packets dropped due to conntrack failure. Either new conntrack entry allocation failed, or protocol helper dropped the packet
 }
 
 func init() {
@@ -39,6 +57,31 @@ func NewConntrackCollector() (Collector, error) {
 		limit: prometheus.NewDesc(
 			prometheus.BuildFQName(namespace, "", "nf_conntrack_entries_limit"),
 			"Maximum size of connection tracking table.",
+			nil, nil,
+		),
+		searched: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, "", "nf_conntrack_stat_searched"),
+			"Number of conntrack table lookups performed.",
+			nil, nil,
+		),
+		insert: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, "", "nf_conntrack_stat_insert"),
+			"Number of entries inserted into the list.",
+			nil, nil,
+		),
+		insertFailed: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, "", "nf_conntrack_stat_insert_failed"),
+			"Number of entries for which list insertion was attempted but failed.",
+			nil, nil,
+		),
+		delete: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, "", "nf_conntrack_stat_delete"),
+			"Number of conntrack entries which were removed.",
+			nil, nil,
+		),
+		drop: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, "", "nf_conntrack_stat_drop"),
+			"Number of packets dropped due to conntrack failure.",
 			nil, nil,
 		),
 	}, nil
@@ -60,5 +103,51 @@ func (c *conntrackCollector) Update(ch chan<- prometheus.Metric) error {
 	ch <- prometheus.MustNewConstMetric(
 		c.limit, prometheus.GaugeValue, float64(value))
 
+	data, err := ioutil.ReadFile(procFilePath("net/stat/nf_conntrack"))
+	if err != nil {
+		return nil
+	}
+	connStat := parseNfConntrackStat(data)
+	ch <- prometheus.MustNewConstMetric(
+		c.searched, prometheus.GaugeValue, float64(connStat.Searched))
+	ch <- prometheus.MustNewConstMetric(
+		c.insert, prometheus.GaugeValue, float64(connStat.Insert))
+	ch <- prometheus.MustNewConstMetric(
+		c.insertFailed, prometheus.GaugeValue, float64(connStat.InsertFailed))
+	ch <- prometheus.MustNewConstMetric(
+		c.delete, prometheus.GaugeValue, float64(connStat.Delete))
+	ch <- prometheus.MustNewConstMetric(
+		c.drop, prometheus.GaugeValue, float64(connStat.Drop))
 	return nil
+}
+
+func parseNfConntrackStat(data []byte) *ConntrackStatistics {
+	connstat := ConntrackStatistics{}
+
+	r := bytes.NewReader(data)
+	scanner := bufio.NewScanner(r)
+
+	for scanner.Scan() {
+		fields := strings.Fields(scanner.Text())
+		parseFields(&connstat, fields)
+	}
+
+	return &connstat
+}
+
+func parseFields(cs *ConntrackStatistics, fields []string) {
+	searched, _ := strconv.ParseUint(fields[1], 16, 64)
+	cs.Searched += searched
+
+	delete, _ := strconv.ParseUint(fields[6], 16, 64)
+	cs.Delete += delete
+
+	insert, _ := strconv.ParseUint(fields[8], 16, 64)
+	cs.Insert += insert
+
+	insertFailed, _ := strconv.ParseUint(fields[9], 16, 64)
+	cs.InsertFailed += insertFailed
+
+	drop, _ := strconv.ParseUint(fields[10], 16, 64)
+	cs.Drop += drop
 }
