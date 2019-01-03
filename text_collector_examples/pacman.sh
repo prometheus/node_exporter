ef39b2ef8582677791eb675c7d0c398f7d65ed27
Julian Kornberger <jk+github@digineo.de>
2019-01-03 22:58:49 +0100

    Change shebang line

--- text_collector_examples/pacman.sh
+++ text_collector_examples/pacman.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
 #
 # Description: Expose metrics from pacman updates
--- text_collector_examples/smartmon.sh
+++ text_collector_examples/smartmon.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # Script informed by the collectd monitoring script for smartmontools (using smartctl)
 # by Samuel B. <samuel_._behan_(at)_dob_._sk> (c) 2012
 # source at: http://devel.dob.sk/collectd-scripts/
