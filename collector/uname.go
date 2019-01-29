460f2f7d70125e28e91a9185cdc8db3764cab18e
David O'Rourke <david.orourke@gmail.com>
2019-01-29 10:46:41 +0000

    collector/uname: Alphabetise +build OSs
    Signed-off-by: David O'Rourke <david.orourke@gmail.com>

--- collector/uname.go
+++ collector/uname.go
@@ -11,7 +11,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build linux freebsd
+// +build freebsd linux
 // +build !nouname
 
 package collector
