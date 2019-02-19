0e48c29311f58a8417b9288152300ae66a2baeb2
FelixDefrance <github@d2france.fr>
2019-02-19 16:16:40 +0100

    added: debug + help
    Signed-off-by: FelixDefrance <github@d2france.fr>

--- text_collector_examples/iptables_collector.py
+++ text_collector_examples/iptables_collector.py
@@ -3,6 +3,7 @@
 import subprocess
 import re
 import os
+import argparse
 
 # Change to your collector.textfile.directory
 prom_outfile = '/var/lib/prometheus/node-exporter/iptables_collector.prom'
@@ -23,16 +24,16 @@ for table in tables:
 
     if re_blankline.match(str(line)):
       continue
-  
+
     line_pieces = line.split()
-  
+
     if re_chain.match(str(line_pieces[0])):
       l_chain_name = line_pieces[1]
-      continue 
-  
+      continue
+
     if re_header.match(str(line_pieces[0])):
       continue
-  
+
     l_packets = line_pieces[0]
     l_bytes = line_pieces[1]
     l_target = line_pieces[2]
@@ -42,17 +43,33 @@ for table in tables:
     l_src = line_pieces[7]
     l_dest = line_pieces[8]
     l_options = ' '.join(line_pieces[9:]).replace('"','\\"')
-  
+
     iptables_packet_lines.append('iptables_packets_total{table="%s",chain="%s",target="%s",prot="%s",in="%s",out="%s",src="%s",dest="%s",opt="%s"} %s' % (table,l_chain_name,l_target,l_prot,l_in,l_out,l_src,l_dest,l_options,l_packets))
     iptables_byte_lines.append('iptables_bytes_total{table="%s",chain="%s",target="%s",prot="%s",in="%s",out="%s",src="%s",dest="%s",opt="%s"} %s' % (table,l_chain_name,l_target,l_prot,l_in,l_out,l_src,l_dest,l_options,l_bytes))
 
-with open(prom_outfile, 'w') as prom_out:
-  print('# HELP iptables_packets_total packet counters for iptable rules.', file=prom_out)
-  print('# TYPE iptables_packets_total counter', file=prom_out)
-  for line in iptables_packet_lines:
-    print(line, file=prom_out)
 
-  print('# HELP iptables_bytes_total byte counters for iptable rules.', file=prom_out)
-  print('# TYPE iptables_bytes_total counter', file=prom_out)
-  for line in iptables_byte_lines:
-    print(line, file=prom_out)
+parser = argparse.ArgumentParser()
+parser.add_argument('--debug', action='store_true', help='debug iptables_collector output')
+args = parser.parse_args()
+
+if args.debug:
+    print('# HELP iptables_packets_total packet counters for iptable rules.')
+    print('# TYPE iptables_packets_total counter')
+    for line in iptables_packet_lines:
+        print(line)
+
+    print('# HELP iptables_bytes_total byte counters for iptable rules.')
+    print('# TYPE iptables_bytes_total counter')
+    for line in iptables_byte_lines:
+        print(line)
+else:
+    with open(prom_outfile, 'w') as prom_out:
+        print('# HELP iptables_packets_total packet counters for iptable rules.', file=prom_out)
+        print('# TYPE iptables_packets_total counter', file=prom_out)
+        for line in iptables_packet_lines:
+            print(line, file=prom_out)
+
+        print('# HELP iptables_bytes_total byte counters for iptable rules.', file=prom_out)
+        print('# TYPE iptables_bytes_total counter', file=prom_out)
+        for line in iptables_byte_lines:
+            print(line, file=prom_out)
