766bf9dac91f520a2d443f48d78561751b7e36b0
Henk <henk@wearespindle.com>
2019-04-08 11:57:25 +0200

    Set label to device instead of disk
    Signed-off-by: Henk <henk@wearespindle.com>

--- text_collector_examples/nvme_metrics.sh
+++ text_collector_examples/nvme_metrics.sh
@@ -37,44 +37,44 @@ for device in ${device_list}; do
 
   # The temperature value in JSON is in Kelvin, we want Celsius
   value_temperature="$(echo "$json_check" | jq '.temperature - 273')"
-  echo "temperature_celcius{disk=\"${disk}\"} ${value_temperature}"
+  echo "temperature_celcius{device=\"${disk}\"} ${value_temperature}"
 
   value_available_spare="$(echo "$json_check" | jq '.avail_spare / 100')"
-  echo "available_spare_ratio{disk=\"${disk}\"} ${value_available_spare}"
+  echo "available_spare_ratio{device=\"${disk}\"} ${value_available_spare}"
 
   value_available_spare_threshold="$(echo "$json_check" | jq '.spare_thresh / 100')"
-  echo "available_spare_threshold_ratio{disk=\"${disk}\"} ${value_available_spare_threshold}"
+  echo "available_spare_threshold_ratio{device=\"${disk}\"} ${value_available_spare_threshold}"
 
   value_percentage_used="$(echo "$json_check" | jq '.percent_used / 100')"
-  echo "percentage_used_ratio{disk=\"${disk}\"} ${value_percentage_used}"
+  echo "percentage_used_ratio{device=\"${disk}\"} ${value_percentage_used}"
 
   value_critical_warning="$(echo "$json_check" | jq '.critical_warning')"
-  echo "critical_warning_total{disk=\"${disk}\"} ${value_critical_warning}"
+  echo "critical_warning_total{device=\"${disk}\"} ${value_critical_warning}"
 
   value_media_errors="$(echo "$json_check" | jq '.media_errors')"
-  echo "media_errors_total{disk=\"${disk}\"} ${value_media_errors}"
+  echo "media_errors_total{device=\"${disk}\"} ${value_media_errors}"
 
   value_num_err_log_entries="$(echo "$json_check" | jq '.num_err_log_entries')"
-  echo "num_err_log_entries_total{disk=\"${disk}\"} ${value_num_err_log_entries}"
+  echo "num_err_log_entries_total{device=\"${disk}\"} ${value_num_err_log_entries}"
 
   value_power_cycles="$(echo "$json_check" | jq '.power_cycles')"
-  echo "power_cycles_total{disk=\"${disk}\"} ${value_power_cycles}"
+  echo "power_cycles_total{device=\"${disk}\"} ${value_power_cycles}"
 
   value_power_on_hours="$(echo "$json_check" | jq '.power_on_hours')"
-  echo "power_on_hours_total{disk=\"${disk}\"} ${value_power_on_hours}"
+  echo "power_on_hours_total{device=\"${disk}\"} ${value_power_on_hours}"
 
   value_controller_busy_time="$(echo "$json_check" | jq '.controller_busy_time')"
-  echo "controller_busy_time_seconds{disk=\"${disk}\"} ${value_controller_busy_time}"
+  echo "controller_busy_time_seconds{device=\"${disk}\"} ${value_controller_busy_time}"
 
   value_data_units_written="$(echo "$json_check" | jq '.data_units_written')"
-  echo "data_units_written_total{disk=\"${disk}\"} ${value_data_units_written}"
+  echo "data_units_written_total{device=\"${disk}\"} ${value_data_units_written}"
 
   value_data_units_read="$(echo "$json_check" | jq '.data_units_read')"
-  echo "data_units_read_total{disk=\"${disk}\"} ${value_data_units_read}"
+  echo "data_units_read_total{device=\"${disk}\"} ${value_data_units_read}"
 
   value_host_read_commands="$(echo "$json_check" | jq '.host_read_commands')"
-  echo "host_read_commands_total{disk=\"${disk}\"} ${value_host_read_commands}"
+  echo "host_read_commands_total{device=\"${disk}\"} ${value_host_read_commands}"
 
   value_host_write_commands="$(echo "$json_check" | jq '.host_write_commands')"
-  echo "host_write_commands_total{disk=\"${disk}\"} ${value_host_write_commands}"
+  echo "host_write_commands_total{device=\"${disk}\"} ${value_host_write_commands}"
 done | format_output
