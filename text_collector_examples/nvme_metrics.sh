0bc85a711597d129569c3337123857a37a3648c7
Henk <henk@wearespindle.com>
2019-04-08 10:11:42 +0200

    Use JSON output in some checks
    Signed-off-by: Henk <henk@wearespindle.com>

--- text_collector_examples/nvme_metrics.sh
+++ text_collector_examples/nvme_metrics.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -eu
 
-# Dependencies: nvme-cli (package)
+# Dependencies: nvme-cli, jq (packages)
 # Based on code from
 # - https://github.com/prometheus/node_exporter/blob/master/text_collector_examples/smartmon.sh
 # - https://github.com/vorlon/check_nvme/blob/master/check_nvme.sh
@@ -30,49 +30,55 @@ echo "nvmecli{version=\"${nvme_version}\"} 1" | format_output
 # Cut device name to /dev/nvmeX
 device_list="$(nvme list | awk '/^\/dev/{print $1}' | cut -c1-10)"
 
+# Loop through the NVMe devices
 for device in ${device_list}; do
-  CHECK=$(nvme smart-log "${device}")
-  DISK="$(echo "${device}" | cut -c6-10)"
+  normal_check="$(nvme smart-log "${device}")"
+  json_check="$(nvme smart-log -o json "${device}")"
+  disk="$(echo "${device}" | cut -c6-10)"
 
-  value_critical_warning=$(echo "$CHECK" | awk '$1 == "critical_warning" {print $3}')
-  echo "critical_warning_total{disk=\"${DISK}\"} ${value_critical_warning}"
+  # Normal output checks ######################################################
+  # Converting percentage to ratio is easier with awk
+  value_available_spare="$(echo "$normal_check" | sed 's/%$//' | awk '$1 == "available_spare" {print $3 / 100}')"
+  echo "available_spare_ratio{disk=\"${disk}\"} ${value_available_spare}"
 
-  value_media_errors=$(echo "$CHECK" | awk '$1 == "media_errors" {print $3}')
-  echo "media_errors_total{disk=\"${DISK}\"} ${value_media_errors}"
+  value_available_spare_threshold="$(echo "$normal_check" | sed 's/%$//' | awk '$1 == "available_spare_threshold" {print $3 / 100}')"
+  echo "available_spare_threshold_ratio{disk=\"${disk}\"} ${value_available_spare_threshold}"
 
-  value_num_err_log_entries=$(echo "$CHECK" | awk '$1 == "num_err_log_entries" {print $3}')
-  echo "num_err_log_entries_total{disk=\"${DISK}\"} ${value_num_err_log_entries}"
+  value_percentage_used="$(echo "$normal_check" | sed 's/%$//' | awk '$1 == "percentage_used" {print $3 / 100}')"
+  echo "percentage_used_ratio{disk=\"${disk}\"} ${value_percentage_used}"
 
-  value_power_cycles=$(echo "$CHECK" | awk '$1 == "power_cycles" {print $3}' | sed 's/,\+//g')
-  echo "power_cycles_total{disk=\"${DISK}\"} ${value_power_cycles}"
+  # The temperature value in JSON is odd, hence checking with "normal"
+  value_temperature="$(echo "$normal_check" | awk '$1 == "temperature" {print $3}')"
+  echo "temperature_celcius{disk=\"${disk}\"} ${value_temperature}"
 
-  value_power_on_hours=$(echo "$CHECK" | awk '$1 == "power_on_hours" {print $3}' | sed 's/,\+//g')
-  echo "power_on_hours_total{disk=\"${DISK}\"} ${value_power_on_hours}"
+  # JSON output checks ########################################################
+  value_critical_warning="$(echo "$json_check" | jq '.critical_warning')"
+  echo "critical_warning_total{disk=\"${disk}\"} ${value_critical_warning}"
 
-  value_temperature=$(echo "$CHECK" | awk '$1 == "temperature" {print $3}')
-  echo "temperature_celcius{disk=\"${DISK}\"} ${value_temperature}"
+  value_media_errors="$(echo "$json_check" | jq '.media_errors')"
+  echo "media_errors_total{disk=\"${disk}\"} ${value_media_errors}"
 
-  value_controller_busy_time=$(echo "$CHECK" | awk '$1 == "controller_busy_time" {print $3}' | sed 's/,\+//g')
-  echo "controller_busy_time_seconds{disk=\"${DISK}\"} ${value_controller_busy_time}"
+  value_num_err_log_entries="$(echo "$json_check" | jq '.num_err_log_entries')"
+  echo "num_err_log_entries_total{disk=\"${disk}\"} ${value_num_err_log_entries}"
 
-  value_available_spare=$(echo "$CHECK" | sed 's/%$//' | awk '$1 == "available_spare" {print $3 / 100}')
-  echo "available_spare_ratio{disk=\"${DISK}\"} ${value_available_spare}"
+  value_power_cycles="$(echo "$json_check" | jq '.power_cycles')"
+  echo "power_cycles_total{disk=\"${disk}\"} ${value_power_cycles}"
 
-  value_available_spare_threshold=$(echo "$CHECK" | sed 's/%$//' | awk '$1 == "available_spare_threshold" {print $3 / 100}')
-  echo "available_spare_threshold_ratio{disk=\"${DISK}\"} ${value_available_spare_threshold}"
+  value_power_on_hours="$(echo "$json_check" | jq '.power_on_hours')"
+  echo "power_on_hours_total{disk=\"${disk}\"} ${value_power_on_hours}"
 
-  value_percentage_used=$(echo "$CHECK" | sed 's/%$//' | awk '$1 == "percentage_used" {print $3 / 100}')
-  echo "percentage_used_ratio{disk=\"${DISK}\"} ${value_percentage_used}"
+  value_controller_busy_time="$(echo "$json_check" | jq '.controller_busy_time')"
+  echo "controller_busy_time_seconds{disk=\"${disk}\"} ${value_controller_busy_time}"
 
-  value_data_units_written=$(echo "$CHECK" | awk '$1 == "data_units_written" {print $3}' | sed 's/,\+//g')
-  echo "data_units_written_total{disk=\"${DISK}\"} ${value_data_units_written}"
+  value_data_units_written="$(echo "$json_check" | jq '.data_units_written')"
+  echo "data_units_written_total{disk=\"${disk}\"} ${value_data_units_written}"
 
-  value_data_units_read=$(echo "$CHECK" | awk '$1 == "data_units_read" {print $3}' | sed 's/,\+//g')
-  echo "data_units_read_total{disk=\"${DISK}\"} ${value_data_units_read}"
+  value_data_units_read="$(echo "$json_check" | jq '.data_units_read')"
+  echo "data_units_read_total{disk=\"${disk}\"} ${value_data_units_read}"
 
-  value_host_read_commands=$(echo "$CHECK" | awk '$1 == "host_read_commands" {print $3}' | sed 's/,\+//g')
-  echo "host_read_commands_total{disk=\"${DISK}\"} ${value_host_read_commands}"
+  value_host_read_commands="$(echo "$json_check" | jq '.host_read_commands')"
+  echo "host_read_commands_total{disk=\"${disk}\"} ${value_host_read_commands}"
 
-  value_host_write_commands=$(echo "$CHECK" | awk '$1 == "host_write_commands" {print $3}' | sed 's/,\+//g')
-  echo "host_write_commands_total{disk=\"${DISK}\"} ${value_host_write_commands}"
+  value_host_write_commands="$(echo "$json_check" | jq '.host_write_commands')"
+  echo "host_write_commands_total{disk=\"${disk}\"} ${value_host_write_commands}"
 done | format_output
