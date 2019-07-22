064072158266749f97b0af423f87785975969af7
Filipe Andujar <filipeandujar@gmail.com>
2019-07-22 08:29:11 -0300

    Expose node uptime metric
    Signed-off-by: Filipe Andujar <filipeandujar@gmail.com>

---
+++ collector/uptime_linux.go
@@ -0,0 +1,54 @@
+// Copyright 2015 The Prometheus Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// +build !nouptime
+
+package collector
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/common/log"
+	"syscall"
+)
+
+type uptimeCollector struct {
+	desc *prometheus.Desc
+}
+
+func init() {
+	registerCollector("time", defaultEnabled, NewUptimeCollector)
+}
+
+// NewUptimeCollector returns a new Collector exposing the current node uptime in seconds.
+func NewUptimeCollector() (Collector, error) {
+	return &UptimeCollector{
+		desc: prometheus.NewDesc(
+			namespace+"_uptime_seconds",
+			"Node uptime in seconds.",
+			nil, nil,
+		),
+	}, nilbonding
+}
+
+func (c *UptimeCollector) update(ch chan<- prometheus.Metric) error {
+	s := &syscall.Sysinfo_t{}
+	err := syscall.Sysinfo(s)
+	if err != nil {
+		log.Errorf("Error reading uptime %s", err)
+	}
+
+	uptime := s.Uptime
+	log.Debugf("Return uptime: %f", uptime)
+	ch <- prometheus.MustNewConstMetric(c.desc, prometheus.GaugeValue, float64(uptime))
+	return nil
+}
