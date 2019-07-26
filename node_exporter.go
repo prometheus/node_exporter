88cc72046ca4e14b05ce12887267205972f59077
Matthias Rampke <mr@soundcloud.com>
2019-07-26 14:31:54 +0000

    Report non-fatal collection errors in the exporter metric.
    As per prometheus/client_golang#543, pass the Registry for exporter
    metrics when setting up the /metrics HTTP handler.
    
    With this, the `promhttp_metric_handler_errors_total` metric will
    increment on (possibly non-fatal) collection-time errors, such as
    duplicate metrics from text files.
    
    Signed-off-by: Matthias Rampke <mr@soundcloud.com>

--- node_exporter.go
+++ node_exporter.go
@@ -116,6 +116,7 @@ func (h *handler) innerHandler(filters ...string) (http.Handler, error) {
 			ErrorLog:            log.NewErrorLogger(),
 			ErrorHandling:       promhttp.ContinueOnError,
 			MaxRequestsInFlight: h.maxRequests,
+			Registry:            h.exporterMetricsRegistry,
 		},
 	)
 	if h.includeExporterMetrics {
