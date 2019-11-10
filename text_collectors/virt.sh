86e32025fbb46713b52465c83893316cd1bdc88e
Clayton Coleman <ccoleman@redhat.com>
2019-11-10 16:55:57 -0500

    text_collectors: Fix virt.sh to include help text
    Overwriting each time vs appending

--- text_collectors/virt.sh
+++ text_collectors/virt.sh
@@ -18,13 +18,13 @@ rm -f "${v}" || true
 touch "${v}"
 if [ -x /usr/sbin/virt-what ]
 then
+  echo '# HELP virt_platform reports one series per detected virtualization type' >>"${v}"
+  echo '# TYPE virt_platform gauge' >>"${v}"
   for platform in $( virt-what ); do
     if [[ -z "${platform}" ]]; then
       continue
     fi
-    echo "# HELP virt_platform reports one series per detected virtualization type" >"${v}"
-    echo "# TYPE virt_platform gauge" >"${v}"
-    echo "virt_platform{type=\"${platform}\"} 1" >"${v}"
+    echo "virt_platform{type=\"${platform}\"} 1" >>"${v}"
   done
 fi
 mv "${v}" virt.prom
