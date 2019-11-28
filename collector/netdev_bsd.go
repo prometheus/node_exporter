08a5810b0486c7dcc18344fdac43a733537c8210
Julian Kornberger <jk+github@digineo.de>
2019-11-28 14:44:15 +0100

    Reduce indentation
    Signed-off-by: Julian Kornberger <jk+github@digineo.de>

--- collector/netdev_bsd.go
+++ collector/netdev_bsd.go
@@ -45,32 +45,34 @@ func getNetDevStats(ignore *regexp.Regexp, accept *regexp.Regexp, logger log.Log
 	defer C.freeifaddrs(ifap)
 
 	for ifa = ifap; ifa != nil; ifa = ifa.ifa_next {
-		if ifa.ifa_addr.sa_family == C.AF_LINK {
-			dev := C.GoString(ifa.ifa_name)
-			if ignore != nil && ignore.MatchString(dev) {
-				level.Debug(logger).Log("msg", "Ignoring device", "device", dev)
-				continue
-			}
-			if accept != nil && !accept.MatchString(dev) {
-				level.Debug(logger).Log("msg", "Ignoring device", "device", dev)
-				continue
-			}
-
-			devStats := map[string]string{}
-			data := (*C.struct_if_data)(ifa.ifa_data)
-
-			devStats["receive_packets"] = convertFreeBSDCPUTime(uint64(data.ifi_ipackets))
-			devStats["transmit_packets"] = convertFreeBSDCPUTime(uint64(data.ifi_opackets))
-			devStats["receive_errs"] = convertFreeBSDCPUTime(uint64(data.ifi_ierrors))
-			devStats["transmit_errs"] = convertFreeBSDCPUTime(uint64(data.ifi_oerrors))
-			devStats["receive_bytes"] = convertFreeBSDCPUTime(uint64(data.ifi_ibytes))
-			devStats["transmit_bytes"] = convertFreeBSDCPUTime(uint64(data.ifi_obytes))
-			devStats["receive_multicast"] = convertFreeBSDCPUTime(uint64(data.ifi_imcasts))
-			devStats["transmit_multicast"] = convertFreeBSDCPUTime(uint64(data.ifi_omcasts))
-			devStats["receive_drop"] = convertFreeBSDCPUTime(uint64(data.ifi_iqdrops))
-			devStats["transmit_drop"] = convertFreeBSDCPUTime(uint64(data.ifi_oqdrops))
-			netDev[dev] = devStats
+		if ifa.ifa_addr.sa_family != C.AF_LINK {
+			continue
 		}
+
+		dev := C.GoString(ifa.ifa_name)
+		if ignore != nil && ignore.MatchString(dev) {
+			level.Debug(logger).Log("msg", "Ignoring device", "device", dev)
+			continue
+		}
+		if accept != nil && !accept.MatchString(dev) {
+			level.Debug(logger).Log("msg", "Ignoring device", "device", dev)
+			continue
+		}
+
+		devStats := map[string]string{}
+		data := (*C.struct_if_data)(ifa.ifa_data)
+
+		devStats["receive_packets"] = convertFreeBSDCPUTime(uint64(data.ifi_ipackets))
+		devStats["transmit_packets"] = convertFreeBSDCPUTime(uint64(data.ifi_opackets))
+		devStats["receive_errs"] = convertFreeBSDCPUTime(uint64(data.ifi_ierrors))
+		devStats["transmit_errs"] = convertFreeBSDCPUTime(uint64(data.ifi_oerrors))
+		devStats["receive_bytes"] = convertFreeBSDCPUTime(uint64(data.ifi_ibytes))
+		devStats["transmit_bytes"] = convertFreeBSDCPUTime(uint64(data.ifi_obytes))
+		devStats["receive_multicast"] = convertFreeBSDCPUTime(uint64(data.ifi_imcasts))
+		devStats["transmit_multicast"] = convertFreeBSDCPUTime(uint64(data.ifi_omcasts))
+		devStats["receive_drop"] = convertFreeBSDCPUTime(uint64(data.ifi_iqdrops))
+		devStats["transmit_drop"] = convertFreeBSDCPUTime(uint64(data.ifi_oqdrops))
+		netDev[dev] = devStats
 	}
 
 	return netDev, nil
--- collector/netdev_openbsd.go
+++ collector/netdev_openbsd.go
@@ -42,31 +42,33 @@ func getNetDevStats(ignore *regexp.Regexp, accept *regexp.Regexp, logger log.Log
 	defer C.freeifaddrs(ifap)
 
 	for ifa = ifap; ifa != nil; ifa = ifa.ifa_next {
-		if ifa.ifa_addr.sa_family == C.AF_LINK {
-			dev := C.GoString(ifa.ifa_name)
-			if ignore != nil && ignore.MatchString(dev) {
-				level.Debug(logger).Log("msg", "Ignoring device", "device", dev)
-				continue
-			}
-			if accept != nil && !accept.MatchString(dev) {
-				level.Debug(logger).Log("msg", "Ignoring device", "device", dev)
-				continue
-			}
-
-			devStats := map[string]string{}
-			data := (*C.struct_if_data)(ifa.ifa_data)
-
-			devStats["receive_packets"] = strconv.Itoa(int(data.ifi_ipackets))
-			devStats["transmit_packets"] = strconv.Itoa(int(data.ifi_opackets))
-			devStats["receive_errs"] = strconv.Itoa(int(data.ifi_ierrors))
-			devStats["transmit_errs"] = strconv.Itoa(int(data.ifi_oerrors))
-			devStats["receive_bytes"] = strconv.Itoa(int(data.ifi_ibytes))
-			devStats["transmit_bytes"] = strconv.Itoa(int(data.ifi_obytes))
-			devStats["receive_multicast"] = strconv.Itoa(int(data.ifi_imcasts))
-			devStats["transmit_multicast"] = strconv.Itoa(int(data.ifi_omcasts))
-			devStats["receive_drop"] = strconv.Itoa(int(data.ifi_iqdrops))
-			netDev[dev] = devStats
+		if ifa.ifa_addr.sa_family != C.AF_LINK {
+			continue
 		}
+
+		dev := C.GoString(ifa.ifa_name)
+		if ignore != nil && ignore.MatchString(dev) {
+			level.Debug(logger).Log("msg", "Ignoring device", "device", dev)
+			continue
+		}
+		if accept != nil && !accept.MatchString(dev) {
+			level.Debug(logger).Log("msg", "Ignoring device", "device", dev)
+			continue
+		}
+
+		devStats := map[string]string{}
+		data := (*C.struct_if_data)(ifa.ifa_data)
+
+		devStats["receive_packets"] = strconv.Itoa(int(data.ifi_ipackets))
+		devStats["transmit_packets"] = strconv.Itoa(int(data.ifi_opackets))
+		devStats["receive_errs"] = strconv.Itoa(int(data.ifi_ierrors))
+		devStats["transmit_errs"] = strconv.Itoa(int(data.ifi_oerrors))
+		devStats["receive_bytes"] = strconv.Itoa(int(data.ifi_ibytes))
+		devStats["transmit_bytes"] = strconv.Itoa(int(data.ifi_obytes))
+		devStats["receive_multicast"] = strconv.Itoa(int(data.ifi_imcasts))
+		devStats["transmit_multicast"] = strconv.Itoa(int(data.ifi_omcasts))
+		devStats["receive_drop"] = strconv.Itoa(int(data.ifi_iqdrops))
+		netDev[dev] = devStats
 	}
 
 	return netDev, nil
