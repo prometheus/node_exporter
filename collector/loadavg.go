cb840b02da8437d7f8263089ff9e67df65de49b0
Bo Tran Van <tranbo9xnd@gmail.com>
2020-05-28 23:26:34 +0700

    feat: implement collect load avg for windows

--- collector/loadavg.go
+++ collector/loadavg.go
@@ -11,7 +11,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build darwin dragonfly freebsd linux netbsd openbsd solaris
+// +build darwin dragonfly freebsd linux netbsd openbsd solaris windows
 // +build !noloadavg
 
 package collector
