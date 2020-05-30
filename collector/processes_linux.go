951c5c34a52b61d5813923af06c6f72cca11ddc4
Bo Tran Van <botv@vccloud.vn>
2020-05-30 10:06:32 +0700

    add collect memory and iocounters of processes for linux

--- collector/processes_linux.go
+++ collector/processes_linux.go
@@ -16,17 +16,31 @@
 package collector
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 
 	"github.com/go-kit/kit/log"
 	"github.com/go-kit/kit/log/level"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/procfs"
+	"github.com/shirou/gopsutil/process"
+)
+
+const (
+	subsystem = "processes"
+)
+
+var (
+	// Remake labels
+	r = strings.NewReplacer(".exe", "", "-", "_", ".", "_")
 )
 
 type processCollector struct {
 	fs          procfs.FS
+	procfs      []*process.Process
 	threadAlloc *prometheus.Desc
 	threadLimit *prometheus.Desc
 	procsState  *prometheus.Desc
@@ -45,9 +59,14 @@ func NewProcessStatCollector(logger log.Logger) (Collector, error) {
 	if err != nil {
 		return nil, fmt.Errorf("failed to open procfs: %w", err)
 	}
-	subsystem := "processes"
+
+	pfs, err := process.Processes()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get procfs: %w", err)
+	}
 	return &processCollector{
-		fs: fs,
+		fs:     fs,
+		procfs: pfs,
 		threadAlloc: prometheus.NewDesc(
 			prometheus.BuildFQName(namespace, subsystem, "threads"),
 			"Allocated threads in system",
@@ -96,6 +115,44 @@ func (c *processCollector) Update(ch chan<- prometheus.Metric) error {
 	ch <- prometheus.MustNewConstMetric(c.pidUsed, prometheus.GaugeValue, float64(pids))
 	ch <- prometheus.MustNewConstMetric(c.pidMax, prometheus.GaugeValue, float64(pidM))
 
+	// Collect metrics of processes
+	procStats, err := c.getAllocatedProcesses()
+	if err != nil {
+		return fmt.Errorf("unable to retrieve number of allocated processes: %q", err)
+	}
+
+	for procName, procInfo := range procStats {
+		// Update memory information
+		for k, v := range procInfo["mem"] {
+			var key = "mem_bytes"
+			if k == "used_percent" {
+				key = "mem_percent"
+			}
+
+			v, _ := strconv.ParseFloat(v, 10)
+			ch <- prometheus.MustNewConstMetric(
+				prometheus.NewDesc(
+					prometheus.BuildFQName(namespace, subsystem, key),
+					"Memory information field kinds of processes.",
+					[]string{"process_name", "parameter_name"}, nil,
+				),
+				prometheus.GaugeValue, v, procName, k,
+			)
+		}
+
+		// Update iocounters information
+		for k, v := range procInfo["iocounters"] {
+			v, _ := strconv.ParseFloat(v, 10)
+			ch <- prometheus.MustNewConstMetric(
+				prometheus.NewDesc(
+					prometheus.BuildFQName(namespace, subsystem, "iocounters_bytes"),
+					fmt.Sprintf("IOCounters information field kinds of processes."),
+					[]string{"process_name", "parameter_name"}, nil,
+				),
+				prometheus.GaugeValue, v, procName, k,
+			)
+		}
+	}
 	return nil
 }
 
@@ -123,4 +180,98 @@ func (c *processCollector) getAllocatedThreads() (int, map[string]int32, int, er
 		thread += stat.NumThreads
 	}
 	return pids, procStates, thread, nil
+}
+
+func (c *processCollector) getAllocatedProcesses() (map[string]map[string]map[string]string, error) {
+	gaProcesses := map[string]map[string]map[string]string{}
+
+	for _, process := range c.procfs {
+
+		// Ignore root pid
+		if process.Pid == 0 {
+			continue
+		}
+
+		// Ignore process can't get name
+		pName, err := process.Name()
+		if err != nil {
+			continue
+		}
+
+		// remove post.fix .exe before save to map
+		// get memory info that process using
+		kName := r.Replace(pName)
+
+		// Make a map store data
+		gaProcesses[kName] = map[string]map[string]string{}
+
+		memInfo, err := getMemoryInfo(process)
+		if err != nil {
+			continue
+		}
+
+		gaProcesses[kName]["mem"] = memInfo
+
+		iocInfo, err := getIOCountersInfo(process)
+		if err != nil {
+			continue
+		}
+		gaProcesses[kName]["iocounters"] = iocInfo
+
+	}
+
+	return gaProcesses, nil
+}
+
+func getMemoryInfo(proc *process.Process) (map[string]string, error) {
+	gMI := map[string]string{}
+	tgMI := map[string]uint64{}
+	memInfo, err := proc.MemoryInfo()
+	if err != nil {
+		return nil, err
+	}
+
+	// parse data
+	memBytes, err := json.Marshal(memInfo)
+	if err != nil {
+		return nil, err
+	}
+	json.Unmarshal(memBytes, &tgMI)
+
+	for k, v := range tgMI {
+		gMI[k] = fmt.Sprintf(`%v`, v)
+	}
+
+	// get memory percent
+	memPerc, err := proc.MemoryPercent()
+	if err != nil {
+		return nil, err
+	}
+	gMI["used_percent"] = fmt.Sprintf(`%v`, memPerc)
+
+	return gMI, nil
+
+}
+
+func getIOCountersInfo(proc *process.Process) (map[string]string, error) {
+	gIOC := map[string]string{}
+	tgIOC := map[string]uint64{}
+	iocInfo, err := proc.IOCounters()
+	if err != nil {
+		return nil, err
+	}
+
+	// parse data
+	iocBytes, err := json.Marshal(iocInfo)
+	if err != nil {
+		return nil, err
+	}
+	json.Unmarshal(iocBytes, &tgIOC)
+
+	for k, v := range tgIOC {
+		gIOC[k] = fmt.Sprintf(`%v`, v)
+	}
+
+	return gIOC, nil
+
 }
