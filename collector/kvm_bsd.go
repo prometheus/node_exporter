cb7ab5119a07f2e3c80a4d1728fa7b6d07c650e6
Jeffrey Stoke <me@arhat.dev>
2020-06-12 10:26:30 +0200

    Fix collectors' build tags
    Signed-off-by: Jeffrey Stoke <me@arhat.dev>

--- collector/kvm_bsd.go
+++ collector/kvm_bsd.go
@@ -11,7 +11,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build !nomeminfo
+// +build !nokvm
 // +build freebsd dragonfly
 
 package collector
