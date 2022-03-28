9155971e07b9ef03d7b87d17fb345367536a8bf1
Ben Kochie <superq@gmail.com>
2022-03-28 17:58:13 +0200

    Update Go modues
    Update to latest releases.
    * Fix up perf collector syntax.
    
    Signed-off-by: Ben Kochie <superq@gmail.com>

--- collector/perf_linux.go
+++ collector/perf_linux.go
@@ -161,8 +161,8 @@ func (c *perfTracepointCollector) update(ch chan<- prometheus.Metric) error {
 // updateCPU is used to update metrics per CPU profiler.
 func (c *perfTracepointCollector) updateCPU(cpu int, ch chan<- prometheus.Metric) error {
 	profiler := c.profilers[cpu]
-	p, err := profiler.Profile()
-	if err != nil {
+	p := &perf.GroupProfileValue{}
+	if err := profiler.Profile(p); err != nil {
 		level.Error(c.logger).Log("msg", "Failed to collect tracepoint profile", "err", err)
 		return err
 	}
@@ -286,8 +286,12 @@ func NewPerfCollector(logger log.Logger) (Collector, error) {
 	for _, cpu := range cpus {
 		// Use -1 to profile all processes on the CPU, see:
 		// man perf_event_open
-		hwProf, err := perf.NewHardwareProfiler(-1, cpu)
-		if err != nil {
+		hwProf, err := perf.NewHardwareProfiler(
+			-1,
+			cpu,
+			perf.AllHardwareProfilers,
+		)
+		if err != nil && !hwProf.HasProfilers() {
 			return nil, err
 		}
 		if err := hwProf.Start(); err != nil {
@@ -296,8 +300,8 @@ func NewPerfCollector(logger log.Logger) (Collector, error) {
 		collector.perfHwProfilers[cpu] = &hwProf
 		collector.hwProfilerCPUMap[&hwProf] = cpu
 
-		swProf, err := perf.NewSoftwareProfiler(-1, cpu)
-		if err != nil {
+		swProf, err := perf.NewSoftwareProfiler(-1, cpu, perf.AllSoftwareProfilers)
+		if err != nil && !swProf.HasProfilers() {
 			return nil, err
 		}
 		if err := swProf.Start(); err != nil {
@@ -306,8 +310,13 @@ func NewPerfCollector(logger log.Logger) (Collector, error) {
 		collector.perfSwProfilers[cpu] = &swProf
 		collector.swProfilerCPUMap[&swProf] = cpu
 
-		cacheProf, err := perf.NewCacheProfiler(-1, cpu)
-		if err != nil {
+		cacheProfilers := perf.L1DataReadHitProfiler & perf.L1DataReadMissProfiler & perf.L1DataWriteHitProfiler & perf.L1InstrReadMissProfiler & perf.InstrTLBReadHitProfiler & perf.InstrTLBReadMissProfiler & perf.LLReadHitProfiler & perf.LLReadMissProfiler & perf.LLWriteHitProfiler & perf.LLWriteMissProfiler & perf.BPUReadHitProfiler & perf.BPUReadMissProfiler
+		cacheProf, err := perf.NewCacheProfiler(
+			-1,
+			cpu,
+			cacheProfilers,
+		)
+		if err != nil && !cacheProf.HasProfilers() {
 			return nil, err
 		}
 		if err := cacheProf.Start(); err != nil {
@@ -585,8 +594,8 @@ func (c *perfCollector) Update(ch chan<- prometheus.Metric) error {
 
 func (c *perfCollector) updateHardwareStats(ch chan<- prometheus.Metric) error {
 	for _, profiler := range c.perfHwProfilers {
-		hwProfile, err := (*profiler).Profile()
-		if err != nil {
+		hwProfile := &perf.HardwareProfile{}
+		if err := (*profiler).Profile(hwProfile); err != nil {
 			return err
 		}
 		if hwProfile == nil {
@@ -657,8 +666,8 @@ func (c *perfCollector) updateHardwareStats(ch chan<- prometheus.Metric) error {
 
 func (c *perfCollector) updateSoftwareStats(ch chan<- prometheus.Metric) error {
 	for _, profiler := range c.perfSwProfilers {
-		swProfile, err := (*profiler).Profile()
-		if err != nil {
+		swProfile := &perf.SoftwareProfile{}
+		if err := (*profiler).Profile(swProfile); err != nil {
 			return err
 		}
 		if swProfile == nil {
@@ -713,8 +722,8 @@ func (c *perfCollector) updateSoftwareStats(ch chan<- prometheus.Metric) error {
 
 func (c *perfCollector) updateCacheStats(ch chan<- prometheus.Metric) error {
 	for _, profiler := range c.perfCacheProfilers {
-		cacheProfile, err := (*profiler).Profile()
-		if err != nil {
+		cacheProfile := &perf.CacheProfile{}
+		if err := (*profiler).Profile(cacheProfile); err != nil {
 			return err
 		}
 		if cacheProfile == nil {
--- go.mod
+++ go.mod
@@ -7,21 +7,21 @@ require (
 	github.com/go-kit/log v0.2.0
 	github.com/godbus/dbus v0.0.0-20190402143921-271e53dc4968
 	github.com/hashicorp/go-envparse v0.0.0-20200406174449-d9cfd743a15e
-	github.com/hodgesds/perf-utils v0.4.0
+	github.com/hodgesds/perf-utils v0.5.1
 	github.com/illumos/go-kstat v0.0.0-20210513183136-173c9b0a9973
-	github.com/jsimonetti/rtnetlink v0.0.0-20211022192332-93da33804786
+	github.com/jsimonetti/rtnetlink v1.1.1
 	github.com/lufia/iostat v1.2.1
 	github.com/mattn/go-xmlrpc v0.0.3
-	github.com/mdlayher/wifi v0.0.0-20200527114002-84f0b9457fdd
-	github.com/prometheus/client_golang v1.11.0
+	github.com/mdlayher/wifi v0.0.0-20220320220353-954ff73a19a5
+	github.com/prometheus/client_golang v1.12.1
 	github.com/prometheus/client_model v0.2.0
-	github.com/prometheus/common v0.32.1
-	github.com/prometheus/exporter-toolkit v0.7.0
+	github.com/prometheus/common v0.33.0
+	github.com/prometheus/exporter-toolkit v0.7.1
 	github.com/prometheus/procfs v0.7.4-0.20211011103944-1a7a2bd3279f
-	github.com/safchain/ethtool v0.1.0
+	github.com/safchain/ethtool v0.2.0
 	github.com/siebenmann/go-kstat v0.0.0-20210513183136-173c9b0a9973 // indirect
 	github.com/soundcloud/go-runit v0.0.0-20150630195641-06ad41a06c4a
-	golang.org/x/sys v0.0.0-20211117180635-dee7805ff2e1
+	golang.org/x/sys v0.0.0-20220330033206-e17cdc41300f
 	gopkg.in/alecthomas/kingpin.v2 v2.2.6
 )
 
