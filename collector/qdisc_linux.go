2bc5bb5f1aad7dde9f88e145d6f165dd524548b6
jbradleynh <jbradley@fastly.com>
2023-08-18 11:27:04 -0400

    import log levels
    Signed-off-by: jbradleynh <jbradley@fastly.com>

--- collector/qdisc_linux.go
+++ collector/qdisc_linux.go
@@ -25,6 +25,7 @@ import (
 	"github.com/alecthomas/kingpin/v2"
 	"github.com/ema/qdisc"
 	"github.com/go-kit/log"
+	"github.com/go-kit/log/level"
 	"github.com/prometheus/client_golang/prometheus"
 )
 
