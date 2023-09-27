fbff3bb21a69ebb3d8f980b11e416fe9b5044d7c
Marc Tuduri <marctc@protonmail.com>
2023-09-27 15:47:39 +0200

    Remove comment

--- collector/config.go
+++ collector/config.go
@@ -11,9 +11,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build !noarp
-// +build !noarp
-
 package collector
 
 type NodeCollectorConfig struct {
