c9c01f7b836dfff508e5df78b366e272e4b09c6b
Caleb Webber <caleb@codingthemsoftly.com>
2024-02-14 03:13:18 -0500

    Revert "Add ZFS freebsd per dataset stats (#2753)" (#2925)
    This reverts commit f34aaa61092fe7e3c6618fdb0b0d16a68a291ff7.
    
    Signed-off-by: Caleb Webber <caleb@codingthemsoftly.com>
    Signed-off-by: Vitaly Zhuravlev <v-zhuravlev@users.noreply.github.com>

--- collector/zfs_linux_test.go
+++ collector/zfs_linux_test.go
@@ -332,7 +332,7 @@ func TestZpoolObjsetParsing(t *testing.T) {
 			t.Fatal(err)
 		}
 
-		err = c.parseLinuxPoolObjsetFile(file, zpoolPath, func(poolName string, datasetName string, s zfsSysctl, v uint64) {
+		err = c.parsePoolObjsetFile(file, zpoolPath, func(poolName string, datasetName string, s zfsSysctl, v uint64) {
 			if s != zfsSysctl("kstat.zfs.misc.objset.writes") {
 				return
 			}
