6235a644c103aae4f93a0ed26df60fea11061788
Chris Cleeland <chris.cleeland@gmail.com>
2024-04-22 15:34:00 -0500

    Fix watchdog_test lint and test failures on macos. (#3003)
    Ensure identical build flags embedded in both files.
    
    Signed-off-by: Chris Cleeland <chris.cleeland@gmail.com>
    Signed-off-by: Vitaly Zhuravlev <v-zhuravlev@users.noreply.github.com>

--- collector/watchdog_test.go
+++ collector/watchdog_test.go
@@ -11,8 +11,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build !nowatchdog
-// +build !nowatchdog
+//go:build linux && !nowatchdog
+// +build linux,!nowatchdog
 
 package collector
 
