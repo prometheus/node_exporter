8ea822f67a257257462553d36c479920bf588c26
Daniel Swarbrick <daniel.swarbrick@gmail.com>
2024-05-12 18:25:06 +0200

    zfs: move common init function out of arch-specific files
    Signed-off-by: Daniel Swarbrick <daniel.swarbrick@gmail.com>

--- collector/zfs_solaris.go
+++ collector/zfs_solaris.go
@@ -61,10 +61,6 @@ const (
 	zfsCollectorSubsystem = "zfs"
 )
 
-func init() {
-	registerCollector("zfs", defaultEnabled, NewZfsCollector)
-}
-
 func NewZfsCollector(logger *slog.Logger) (Collector, error) {
 	return &zfsCollector{
 		abdstatsLinearCount: prometheus.NewDesc(
