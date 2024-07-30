88ed2cecc301ab0fbabf55daf20c428188d15103
Johannes Ziemke <github@5pi.de>
2024-07-30 15:55:41 +0200

    Add AIX loadavg collector
    Signed-off-by: Johannes Ziemke <github@5pi.de>

--- collector/loadavg.go
+++ collector/loadavg.go
@@ -11,8 +11,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build (darwin || dragonfly || freebsd || linux || netbsd || openbsd || solaris) && !noloadavg
-// +build darwin dragonfly freebsd linux netbsd openbsd solaris
+//go:build (darwin || dragonfly || freebsd || linux || netbsd || openbsd || solaris || aix) && !noloadavg
+// +build darwin dragonfly freebsd linux netbsd openbsd solaris aix
 // +build !noloadavg
 
 package collector
