a703690707f3f649b3f5a5b7399f5a74e62d43a6
Johannes Ziemke <github@5pi.de>
2024-08-12 12:35:08 +0200

    Enable uname collector on AIX
    Signed-off-by: Johannes Ziemke <github@5pi.de>
    Signed-off-by: Vitaly Zhuravlev <v-zhuravlev@users.noreply.github.com>

--- collector/uname_bsd.go
+++ collector/uname_bsd.go
@@ -11,8 +11,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build (darwin || freebsd || openbsd || netbsd) && !nouname
-// +build darwin freebsd openbsd netbsd
+//go:build (darwin || freebsd || openbsd || netbsd || aix) && !nouname
+// +build darwin freebsd openbsd netbsd aix
 // +build !nouname
 
 package collector
