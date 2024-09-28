72456dcaf22c0e1e2d3cc5d59b4bc2d0fe0f3186
Johannes Ziemke <github@5pi.de>
2024-09-28 11:59:33 +0200

    Disable osrelease collector on aix
    Signed-off-by: Johannes Ziemke <github@5pi.de>
    Signed-off-by: Vitaly Zhuravlev <v-zhuravlev@users.noreply.github.com>

--- collector/os_release.go
+++ collector/os_release.go
@@ -11,6 +11,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//go:build !noosrelease && !aix
+// +build !noosrelease,!aix
+
 package collector
 
 import (
