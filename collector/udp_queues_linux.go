13f019f82272bb3b287a67b42ac788e7f214c9a3
Chris Cleeland <chris.cleeland@gmail.com>
2025-01-06 23:05:45 -0600

    Break udp_queues into udp_{queues,drops_total}
    This separates the gauges related to udp_queues and the counter
    related to the total number of drops, but keeps them under the same
    namespace.
    
    Signed-off-by: Chris Cleeland <chris.cleeland@gmail.com>

--- collector/udp_queues_linux.go
+++ collector/udp_queues_linux.go
@@ -27,42 +27,48 @@ import (
 )
 
 type (
-	udpQueuesCollector struct {
+	udpCollector struct {
 		fs     procfs.FS
-		desc   *prometheus.Desc
+		queues *prometheus.Desc
+		drops  *prometheus.Desc
 		logger *slog.Logger
 	}
 )
 
 func init() {
-	registerCollector("udp_queues", defaultEnabled, NewUDPqueuesCollector)
+	registerCollector("udp", defaultEnabled, NewUDPCollector)
 }
 
 // NewUDPqueuesCollector returns a new Collector exposing network udp queued bytes.
-func NewUDPqueuesCollector(logger *slog.Logger) (Collector, error) {
+func NewUDPCollector(logger *slog.Logger) (Collector, error) {
 	fs, err := procfs.NewFS(*procPath)
 	if err != nil {
 		return nil, fmt.Errorf("failed to open procfs: %w", err)
 	}
-	return &udpQueuesCollector{
+	return &udpCollector{
 		fs: fs,
-		desc: prometheus.NewDesc(
+		queues: prometheus.NewDesc(
 			prometheus.BuildFQName(namespace, "udp", "queues"),
 			"Number of allocated memory in the kernel for UDP datagrams in bytes.",
 			[]string{"queue", "ip"}, nil,
+		),
+		drops: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, "udp", "drops_total"),
+			"Total number of datagrams dropped.",
+			[]string{"ip"}, nil,
 		),
 		logger: logger,
 	}, nil
 }
 
-func (c *udpQueuesCollector) Update(ch chan<- prometheus.Metric) error {
+func (c *udpCollector) Update(ch chan<- prometheus.Metric) error {
 
 	s4, errIPv4 := c.fs.NetUDPSummary()
 	if errIPv4 == nil {
-		ch <- prometheus.MustNewConstMetric(c.desc, prometheus.GaugeValue, float64(s4.TxQueueLength), "tx", "v4")
-		ch <- prometheus.MustNewConstMetric(c.desc, prometheus.GaugeValue, float64(s4.RxQueueLength), "rx", "v4")
+		ch <- prometheus.MustNewConstMetric(c.queues, prometheus.GaugeValue, float64(s4.TxQueueLength), "tx", "v4")
+		ch <- prometheus.MustNewConstMetric(c.queues, prometheus.GaugeValue, float64(s4.RxQueueLength), "rx", "v4")
 		if s4.Drops != nil {
-			ch <- prometheus.MustNewConstMetric(c.desc, prometheus.CounterValue, float64(*s4.Drops), "drops", "v4")
+			ch <- prometheus.MustNewConstMetric(c.drops, prometheus.CounterValue, float64(*s4.Drops), "v4")
 		}
 	} else {
 		if errors.Is(errIPv4, os.ErrNotExist) {
@@ -74,10 +80,10 @@ func (c *udpQueuesCollector) Update(ch chan<- prometheus.Metric) error {
 
 	s6, errIPv6 := c.fs.NetUDP6Summary()
 	if errIPv6 == nil {
-		ch <- prometheus.MustNewConstMetric(c.desc, prometheus.GaugeValue, float64(s6.TxQueueLength), "tx", "v6")
-		ch <- prometheus.MustNewConstMetric(c.desc, prometheus.GaugeValue, float64(s6.RxQueueLength), "rx", "v6")
+		ch <- prometheus.MustNewConstMetric(c.queues, prometheus.GaugeValue, float64(s6.TxQueueLength), "tx", "v6")
+		ch <- prometheus.MustNewConstMetric(c.queues, prometheus.GaugeValue, float64(s6.RxQueueLength), "rx", "v6")
 		if s6.Drops != nil {
-			ch <- prometheus.MustNewConstMetric(c.desc, prometheus.CounterValue, float64(*s6.Drops), "drops", "v6")
+			ch <- prometheus.MustNewConstMetric(c.drops, prometheus.CounterValue, float64(*s6.Drops), "v6")
 		}
 	} else {
 		if errors.Is(errIPv6, os.ErrNotExist) {
