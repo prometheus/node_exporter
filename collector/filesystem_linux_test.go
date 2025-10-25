prometheus/node_exporter

commit 434fb7d864fe960f67681fdcf973e48aecf4a0d7

filesystem: take super options into account for read-only (#3387)
* filesystem: take super options into account for read-only

With the latest change implemented to use `mountinfo` instead of
`mounts` there was a regression in filesystem readonly detection
due to super options not taken into account: filesystems that would
previously be marked a "read-only" would not anymore because that
information had moved to super options instead of mount options on
certain occasions.

fixes #3157

Signed-off-by: nicbaz <932244+nicbaz@users.noreply.github.com>

* filesystem: use faster integer to string implementation

Signed-off-by: nicbaz <932244+nicbaz@users.noreply.github.com>

---------

Signed-off-by: nicbaz <932244+nicbaz@users.noreply.github.com>

collector/filesystem_linux.go
@@ -23,6 +23,8 @@ import (
 	"io"
 	"log/slog"
 	"os"
+	"slices"
+	"strconv"
 	"strings"
 	"sync"
 	"time"
@@ -110,11 +112,8 @@ func (c *filesystemCollector) GetStats() ([]filesystemStats, error) {
 
 func (c *filesystemCollector) processStat(labels filesystemLabels) filesystemStats {
 	var ro float64
-	for _, option := range strings.Split(labels.options, ",") {
-		if option == "ro" {
-			ro = 1
-			break
-		}
+	if isFilesystemReadOnly(labels) {
+		ro = 1
 	}
 
 	success := make(chan struct{})
@@ -198,7 +197,7 @@ func parseFilesystemLabels(r io.Reader) ([]filesystemLabels, error) {
 	for scanner.Scan() {
 		parts := strings.Fields(scanner.Text())
 
-		if len(parts) < 10 {
+		if len(parts) < 11 {
 			return nil, fmt.Errorf("malformed mount point information: %q", scanner.Text())
 		}
 
@@ -219,15 +218,26 @@ func parseFilesystemLabels(r io.Reader) ([]filesystemLabels, error) {
 		parts[4] = strings.ReplaceAll(parts[4], "\\011", "\t")
 
 		filesystems = append(filesystems, filesystemLabels{
-			device:      parts[m+3],
-			mountPoint:  rootfsStripPrefix(parts[4]),
-			fsType:      parts[m+2],
-			options:     parts[5],
-			major:       fmt.Sprint(major),
-			minor:       fmt.Sprint(minor),
-			deviceError: "",
+			device:       parts[m+3],
+			mountPoint:   rootfsStripPrefix(parts[4]),
+			fsType:       parts[m+2],
+			mountOptions: parts[5],
+			superOptions: parts[10],
+			major:        strconv.Itoa(major),
+			minor:        strconv.Itoa(minor),
+			deviceError:  "",
 		})
 	}
 
 	return filesystems, scanner.Err()
+}
+
+// see https://github.com/prometheus/node_exporter/issues/3157#issuecomment-2422761187
+// if either mount or super options contain "ro" the filesystem is read-only
+func isFilesystemReadOnly(labels filesystemLabels) bool {
+	if slices.Contains(strings.Split(labels.mountOptions, ","), "ro") || slices.Contains(strings.Split(labels.superOptions, ","), "ro") {
+		return true
+	}
+
+	return false
 }

collector/filesystem_linux_test.go
@@ -45,6 +45,45 @@ func Test_parseFilesystemLabelsError(t *testing.T) {
 	}
 }
 
+func Test_isFilesystemReadOnly(t *testing.T) {
+	tests := map[string]struct {
+		labels   filesystemLabels
+		expected bool
+	}{
+		"/media/volume1": {
+			labels: filesystemLabels{
+				mountOptions: "rw,nosuid,nodev,noexec,relatime",
+				superOptions: "rw,devices",
+			},
+			expected: false,
+		},
+		"/media/volume2": {
+			labels: filesystemLabels{
+				mountOptions: "ro,relatime",
+				superOptions: "rw,fd=22,pgrp=1,timeout=300,minproto=5,maxproto=5,direct",
+			}, expected: true,
+		},
+		"/media/volume3": {
+			labels: filesystemLabels{
+				mountOptions: "rw,user_id=1000,group_id=1000",
+				superOptions: "ro",
+			}, expected: true,
+		},
+		"/media/volume4": {
+			labels: filesystemLabels{
+				mountOptions: "ro,nosuid,noexec",
+				superOptions: "ro,nodev",
+			}, expected: true,
+		},
+	}
+
+	for _, tt := range tests {
+		if got := isFilesystemReadOnly(tt.labels); got != tt.expected {
+			t.Errorf("Expected %t, got %t", tt.expected, got)
+		}
+	}
+}
+
 func TestMountPointDetails(t *testing.T) {
 	if _, err := kingpin.CommandLine.Parse([]string{"--path.procfs", "./fixtures/proc"}); err != nil {
 		t.Fatal(err)

collector/filesystem_common.go
@@ -82,7 +82,7 @@ type filesystemCollector struct {
 }
 
 type filesystemLabels struct {
-	device, mountPoint, fsType, options, deviceError, major, minor string
+	device, mountPoint, fsType, mountOptions, superOptions, deviceError, major, minor string
 }
 
 type filesystemStats struct {

commit 5487b1010c3bfc282d1e16dcd1f2fcb193ff5d55

filesystem: Refactor mountinfo parsing
Migrate mountinfo parsing to procfs library. This fixes incorrect
parsing introduced by PR #3387.

Fixes: https://github.com/prometheus/node_exporter/issues/3450

Signed-off-by: Ben Kochie <superq@gmail.com>

collector/filesystem_linux.go
@@ -17,10 +17,9 @@
 package collector
 
 import (
-	"bufio"
+	"bytes"
 	"errors"
 	"fmt"
-	"io"
 	"log/slog"
 	"os"
 	"slices"
@@ -31,6 +30,8 @@ import (
 
 	"github.com/alecthomas/kingpin/v2"
 	"golang.org/x/sys/unix"
+
+	"github.com/prometheus/procfs"
 )
 
 const (
@@ -179,60 +180,51 @@ func stuckMountWatcher(mountPoint string, success chan struct{}, logger *slog.Lo
 }
 
 func mountPointDetails(logger *slog.Logger) ([]filesystemLabels, error) {
-	file, err := os.Open(procFilePath("1/mountinfo"))
+	fs, err := procfs.NewFS(*procPath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open procfs: %w", err)
+	}
+	mountInfo, err := fs.GetProcMounts(1)
 	if errors.Is(err, os.ErrNotExist) {
 		// Fallback to `/proc/self/mountinfo` if `/proc/1/mountinfo` is missing due hidepid.
 		logger.Debug("Reading root mounts failed, falling back to self mounts", "err", err)
-		file, err = os.Open(procFilePath("self/mountinfo"))
+		mountInfo, err = fs.GetMounts()
 	}
 	if err != nil {
 		return nil, err
 	}
-	defer file.Close()
 
-	return parseFilesystemLabels(file)
+	return parseFilesystemLabels(mountInfo)
 }
 
-func parseFilesystemLabels(r io.Reader) ([]filesystemLabels, error) {
+func parseFilesystemLabels(mountInfo []*procfs.MountInfo) ([]filesystemLabels, error) {
 	var filesystems []filesystemLabels
 
-	scanner := bufio.NewScanner(r)
-	for scanner.Scan() {
-		parts := strings.Fields(scanner.Text())
-
-		if len(parts) < 11 {
-			return nil, fmt.Errorf("malformed mount point information: %q", scanner.Text())
-		}
-
+	for _, mount := range mountInfo {
 		major, minor := 0, 0
-		_, err := fmt.Sscanf(parts[2], "%d:%d", &major, &minor)
+		_, err := fmt.Sscanf(mount.MajorMinorVer, "%d:%d", &major, &minor)
 		if err != nil {
-			return nil, fmt.Errorf("malformed mount point information: %q", scanner.Text())
-		}
-
-		m := 5
-		for parts[m+1] != "-" {
-			m++
+			return nil, fmt.Errorf("malformed mount point MajorMinorVer: %q", mount.MajorMinorVer)
 		}
 
 		// Ensure we handle the translation of \040 and \011
 		// as per fstab(5).
-		parts[4] = strings.ReplaceAll(parts[4], "\\040", " ")
-		parts[4] = strings.ReplaceAll(parts[4], "\\011", "\t")
+		mount.MountPoint = strings.ReplaceAll(mount.MountPoint, "\\040", " ")
+		mount.MountPoint = strings.ReplaceAll(mount.MountPoint, "\\011", "\t")
 
 		filesystems = append(filesystems, filesystemLabels{
-			device:       parts[m+3],
-			mountPoint:   rootfsStripPrefix(parts[4]),
-			fsType:       parts[m+2],
-			mountOptions: parts[5],
-			superOptions: parts[10],
+			device:       mount.Source,
+			mountPoint:   rootfsStripPrefix(mount.MountPoint),
+			fsType:       mount.FSType,
+			mountOptions: mountOptionsString(mount.Options),
+			superOptions: mountOptionsString(mount.SuperOptions),
 			major:        strconv.Itoa(major),
 			minor:        strconv.Itoa(minor),
 			deviceError:  "",
 		})
 	}
 
-	return filesystems, scanner.Err()
+	return filesystems, nil
 }
 
 // see https://github.com/prometheus/node_exporter/issues/3157#issuecomment-2422761187
@@ -243,4 +235,16 @@ func isFilesystemReadOnly(labels filesystemLabels) bool {
 	}
 
 	return false
+}
+
+func mountOptionsString(m map[string]string) string {
+	b := new(bytes.Buffer)
+	for key, value := range m {
+		if value == "" {
+			fmt.Fprintf(b, "%s", key)
+		} else {
+			fmt.Fprintf(b, "%s=%s", key, value)
+		}
+	}
+	return b.String()
 }

collector/fixtures/proc/1/mountinfo
@@ -29,3 +29,4 @@
 3147 3002 0:81 / /run/user/1000/gvfs rw,nosuid,nodev,relatime shared:1290 - fuse.gvfsd-fuse gvfsd-fuse rw,user_id=1000,group_id=1000
 3148 3003 260:0 / /var/lib/kubelet/plugins/kubernetes.io/vsphere-volume/mounts/[vsanDatastore]\040bafb9e5a-8856-7e6c-699c-801844e77a4a/kubernetes-dynamic-pvc-3eba5bba-48a3-11e8-89ab-005056b92113.vmdk rw,relatime shared:31 - ext4 /dev/sda rw,data=ordered
 3149 3004 260:0 / /var/lib/kubelet/plugins/kubernetes.io/vsphere-volume/mounts/[vsanDatastore]\011bafb9e5a-8856-7e6c-699c-801844e77a4a/kubernetes-dynamic-pvc-3eba5bba-48a3-11e8-89ab-005056b92113.vmdk rw,relatime shared:32 - ext4 /dev/sda rw,data=ordered
+1128 67 253:0 /var/lib/containers/storage/overlay /var/lib/containers/storage/overlay rw,relatime - xfs /dev/mapper/rhel-root rw,seclabel,attr2,inode64,logbufs=8,logbsize=32k,noquota

go.mod
@@ -25,7 +25,7 @@ require (
 	github.com/prometheus/client_model v0.6.2
 	github.com/prometheus/common v0.67.1
 	github.com/prometheus/exporter-toolkit v0.14.1
-	github.com/prometheus/procfs v0.18.0
+	github.com/prometheus/procfs v0.19.0
 	github.com/safchain/ethtool v0.6.2
 	golang.org/x/exp v0.0.0-20250911091902-df9299821621
 	golang.org/x/sys v0.37.0

go.sum
@@ -80,8 +80,8 @@ github.com/prometheus/common v0.67.1 h1:OTSON1P4DNxzTg4hmKCc37o4ZAZDv0cfXLkOt0oE
 github.com/prometheus/common v0.67.1/go.mod h1:RpmT9v35q2Y+lsieQsdOh5sXZ6ajUGC8NjZAmr8vb0Q=
 github.com/prometheus/exporter-toolkit v0.14.1 h1:uKPE4ewweVRWFainwvAcHs3uw15pjw2dk3I7b+aNo9o=
 github.com/prometheus/exporter-toolkit v0.14.1/go.mod h1:di7yaAJiaMkcjcz48f/u4yRPwtyuxTU5Jr4EnM2mhtQ=
-github.com/prometheus/procfs v0.18.0 h1:2QTA9cKdznfYJz7EDaa7IiJobHuV7E1WzeBwcrhk0ao=
-github.com/prometheus/procfs v0.18.0/go.mod h1:M0aotyiemPhBCM0z5w87kL22CxfcH05ZpYlu+b4J7mw=
+github.com/prometheus/procfs v0.19.0 h1:2gU9KiEMZUhDokz1/0GToOjT7ljqxHi+GhEjk9UUMgU=
+github.com/prometheus/procfs v0.19.0/go.mod h1:M0aotyiemPhBCM0z5w87kL22CxfcH05ZpYlu+b4J7mw=
 github.com/rogpeppe/go-internal v1.10.0 h1:TMyTOH3F/DB16zRVcYyreMH6GnZZrwQVAoYjRBZyWFQ=
 github.com/rogpeppe/go-internal v1.10.0/go.mod h1:UQnix2H7Ngw/k4C5ijL5+65zddjncjaFoBhdsK/akog=
 github.com/safchain/ethtool v0.6.2 h1:O3ZPFAKEUEfbtE6J/feEe2Ft7dIJ2Sy8t4SdMRiIMHY=

collector/filesystem_linux_test.go
@@ -19,26 +19,31 @@ package collector
 import (
 	"io"
 	"log/slog"
-	"strings"
 	"testing"
 
 	"github.com/alecthomas/kingpin/v2"
+
+	"github.com/prometheus/procfs"
 )
 
 func Test_parseFilesystemLabelsError(t *testing.T) {
 	tests := []struct {
 		name string
-		in   string
+		in   []*procfs.MountInfo
 	}{
 		{
-			name: "too few fields",
-			in:   "hello world",
+			name: "malformed Major:Minor",
+			in: []*procfs.MountInfo{
+				{
+					MajorMinorVer: "nope",
+				},
+			},
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if _, err := parseFilesystemLabels(strings.NewReader(tt.in)); err == nil {
+			if _, err := parseFilesystemLabels(tt.in); err == nil {
 				t.Fatal("expected an error, but none occurred")
 			}
 		})
@@ -120,6 +125,7 @@ func TestMountPointDetails(t *testing.T) {
 		"/run/user/1000/gvfs":             "",
 		"/var/lib/kubelet/plugins/kubernetes.io/vsphere-volume/mounts/[vsanDatastore] bafb9e5a-8856-7e6c-699c-801844e77a4a/kubernetes-dynamic-pvc-3eba5bba-48a3-11e8-89ab-005056b92113.vmdk": "",
 		"/var/lib/kubelet/plugins/kubernetes.io/vsphere-volume/mounts/[vsanDatastore]	bafb9e5a-8856-7e6c-699c-801844e77a4a/kubernetes-dynamic-pvc-3eba5bba-48a3-11e8-89ab-005056b92113.vmdk": "",
+		"/var/lib/containers/storage/overlay": "",
 	}
 
 	filesystems, err := mountPointDetails(slog.New(slog.NewTextHandler(io.Discard, nil)))